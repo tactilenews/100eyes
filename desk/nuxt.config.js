2c0c62d733489907995097e5ce7dde6babb5edfb
roschaefer <git@roschaefer.de>
2020-03-24 16:55:34 +0100

    chore: disable annoying overlay on lint errors

--- desk/nuxt.config.js
+++ desk/nuxt.config.js
@@ -75,6 +75,11 @@ export default {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {}
+    extend(config, ctx) {},
+    hotMiddleware: {
+      client: {
+        overlay: false
+      }
+    }
   }
 }
