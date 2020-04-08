819c2e2ac4d5b7bc300b48e64ddbc75c172a9bdb
Till Prochaska <mail@tillprochaska.de>
2020-04-08 19:32:05 +0200

    Use textarea for question

---
+++ app/components/textarea/textarea.rb
@@ -0,0 +1,25 @@
+module Textarea
+  class Textarea < ViewComponent::Base
+    def initialize(id: nil, value: nil, placeholder: nil, required: false)
+      @id = id
+      @value = value
+      @placeholder = placeholder
+      @required = required
+    end
+
+    def call
+      content_tag(
+        :textarea,
+        value,
+        id: id,
+        class: 'c-textarea',
+        required: required,
+        placeholder: placeholder
+      )
+    end
+
+    private
+
+    attr_reader :id, :value, :placeholder, :required
+  end
+end
