5abeee8b48b17e9f458252daf6bd13046889d4df
Till Prochaska <mail@tillprochaska.de>
2020-04-29 15:35:08 +0200

    Improve form element styles

--- app/components/input/input.rb
+++ app/components/input/input.rb
@@ -2,12 +2,29 @@
 
 module Input
   class Input < ApplicationComponent
-    def initialize(label:)
-      @label = label
+    def initialize(id: nil, value: nil, placeholder: nil, required: false, type: 'text', styles: [])
+      super(styles: styles)
+
+      @id = id
+      @value = value
+      @placeholder = placeholder
+      @required = required
+      @type = type
     end
 
-    def id
-      @label.parameterize
+    def call
+      content_tag(
+        :input,
+        nil,
+        id: id,
+        name: id,
+        value: value,
+        class: class_names,
+        required: required,
+        placeholder: placeholder
+      )
     end
+
+    attr_reader :id, :value, :placeholder, :required, :type
   end
 end
