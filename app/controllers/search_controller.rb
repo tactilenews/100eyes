2396ee8dde6e3627af8d624bfdcd413cc4ae3e97
roschaefer <git@roschaefer.de>
2020-05-07 17:04:50 +0200

    Provide links for replies and users

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,6 +4,6 @@ class SearchController < ApplicationController
   def index
     @results = []
     query = params[:q]
-    @results = PgSearch.multisearch(query) if query
+    @results = PgSearch.multisearch(query).map(&:searchable) if query
   end
 end
