cf9cf74dab077595da5eb5c524ae1aa65a9b7240
roschaefer <git@roschaefer.de>
2020-05-13 15:16:42 +0200

    Fix webpacker:compile task in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,4 +103,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  # CUSTOM
+  config.bot_id = (ENV['BOT'] || :default).to_sym
 end
