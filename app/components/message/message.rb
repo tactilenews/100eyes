263a4c1085d70e7469a36e17068cf2730c09cb0f
roschaefer <git@roschaefer.de>
2020-05-14 15:07:20 +0200

    Add avatar to message component

--- app/components/message/message.rb
+++ app/components/message/message.rb
@@ -4,12 +4,25 @@ module Message
   class Message < ViewComponent::Base
     include ComponentHelper
 
-    def initialize(message:)
+    def initialize(message:, user: nil)
       @message = message
+      @user = user
     end
 
     private
 
-    attr_reader :message
+    attr_reader :message, :user
+
+    def css_class
+      return 'Message Message-right' if user
+
+      'Message Message-left'
+    end
+
+    def avatar_url
+      return user.avatar_url if user
+
+      nil
+    end
   end
 end
