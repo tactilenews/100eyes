96a6d3faf8057f135b412d27cfba66097f850656
roschaefer <git@roschaefer.de>
2020-05-15 17:39:10 +0200

    refactor: scrap dashboard and tile components
    Link to not implemented page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root to: redirect('/users')
   get '/search', to: 'search#index'
-  root to: redirect('/dashboard')
   telegram_webhook Telegram::WebhookController, Rails.configuration.bot_id
   resources :requests, only: %i[new create]
   get 'pending/not_implemented'
-  get 'dashboard', to: 'dashboard#index'
   resources :users, only: %i[index edit update destroy show] do
     resources :requests, only: %i[show]
   end
