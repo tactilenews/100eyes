af6795f526eb1fb592e6dbf0883fda53133605ea
roschaefer <git@roschaefer.de>
2020-05-18 16:09:25 +0200

    Properly configure `reply_to` and `from` for now

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: Rails.configuration.mailer[:from], reply_to: Rails.configuration.mailer[:from]
+  default from: Rails.configuration.mailer[:from], reply_to: Rails.configuration.mailer[:reply_to]
   layout 'mailer'
 end
