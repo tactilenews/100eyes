6ad3c9bd4d304a2b590b117600e92b3ef4613399
Till Prochaska <mail@tillprochaska.de>
2020-05-22 14:52:36 +0200

    Update tests

--- spec/routing/users_routing_spec.rb
+++ spec/routing/users_routing_spec.rb
@@ -8,10 +8,6 @@ RSpec.describe UsersController, type: :routing do
       expect(get: '/users').to route_to('users#index')
     end
 
-    it 'routes to #edit' do
-      expect(get: '/users/1/edit').to route_to('users#edit', id: '1')
-    end
-
     it 'routes to #update via PUT' do
       expect(put: '/users/1').to route_to('users#update', id: '1')
     end
