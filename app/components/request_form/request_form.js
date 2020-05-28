ff6cb83b6f6b4fe6d5506d6b2f6e63acb58edc5b
Till Prochaska <mail@tillprochaska.de>
2020-05-28 15:39:05 +0200

    Update text modules (close #124, close #112)

--- app/components/request_form/request_form.js
+++ app/components/request_form/request_form.js
@@ -2,11 +2,11 @@ import { Controller } from 'stimulus';
 import sanitize from '../../../frontend/helpers/sanitize.js';
 
 const NOTE_TEXTS = {
-  photo: 'Textbaustein für Fotonutzung',
-  address: 'Textbaustein für Addressnutzung',
-  contact: 'Textbaustein für Kontaktweitergabe',
-  medicalInfo: 'Textbaustein für medizinische Informationen',
-  confidential: 'Textbaustein für vertrauliche Informationen',
+  photo: 'Schicken Sie uns doch auch ein Foto (oder mehrere). Bitte schicken Sie nur Fotos, die Sie selber gemacht haben. Mit der Zusendung von Fotos geben Sie gleichzeitig Ihr Einverständnis für eine mögliche Veröffentlichung. Bei Veröffentlichung nennen wir Sie als Urheber (Foto: Max Mustermann/100eyes).',
+  address: 'Schicken Sie uns doch eine Adresse oder genaue Koordinaten. Wir nutzen die Geodaten für unsere Recherchen und werden sie eventuell in einem Text verarbeiten oder als Datenpunkt auf einer Karte verwenden.',
+  contact: 'Schicken Sie uns gerne Kontaktdaten. Wir nutzen die Kontaktdaten für unsere Recherchen und werden die Person eventuell kontaktieren, aber keine Kontaktdaten veröffentlichen.',
+  medicalInfo: 'Sie stellen uns hier möglicherweise sensible medizinische Informationen bereit. Wir sind uns dessen bewusst und behandeln Ihre Daten mit aller gebotenen Vorsicht. Wenn Sie uns Daten schicken, helfen uns diese Informationen für unsere weiteren Recherchen. Ihre Daten werden wir nur intern auswerten, aber nicht veröffentlichen. Es sei denn, Sie geben Ihr ausdrückliches Einverständnis, nachdem wir Sie separat darum gebeten haben.',
+  confidential: 'Sie stellen uns hier möglicherweise vertrauliche Informationen bereit. Wir sind uns dessen bewusst und behandeln diese Informationen mit aller gebotenen Vorsicht. Wir werden Ihre Informationen nur intern auswerten, aber nicht ungefragt veröffentlichen.',
 };
 
 const template = ({ message, notes }) => {
@@ -16,8 +16,8 @@ const template = ({ message, notes }) => {
     .filter(([key, isActive]) => isActive)
     .map(([key]) => NOTE_TEXTS[key]);
 
-  const intro = 'Hallo, die Redaktion hat eine neue Frage an dich:';
-  const outro = 'Vielen Dank für deine Hilfe bei unserer Recherche!';
+  const intro = 'Hallo, die Redaktion hat eine neue Frage an Sie:';
+  const outro = 'Vielen Dank für Ihre Hilfe bei unserer Recherche!';
 
   return [intro, message, ...notes, outro].join('\n\n');
 };
--- app/models/request.rb
+++ app/models/request.rb
@@ -12,19 +12,19 @@ class Request < ApplicationRecord
   end
 
   HINT_TEXTS = {
-    photo: 'Textbaustein für Foto',
-    address: 'Textbaustein für Adressweitergabe',
-    contact: 'Textbaustein für Kontaktweitergabe',
-    medicalInfo: 'Textbaustein für medizinische Informationen',
-    confidential: 'Textbaustein für vertrauliche Informationen'
+    photo: 'Schicken Sie uns doch auch ein Foto (oder mehrere). Bitte schicken Sie nur Fotos, die Sie selber gemacht haben. Mit der Zusendung von Fotos geben Sie gleichzeitig Ihr Einverständnis für eine mögliche Veröffentlichung. Bei Veröffentlichung nennen wir Sie als Urheber (Foto: Max Mustermann/100eyes).',
+    address: 'Schicken Sie uns doch eine Adresse oder genaue Koordinaten. Wir nutzen die Geodaten für unsere Recherchen und werden sie eventuell in einem Text verarbeiten oder als Datenpunkt auf einer Karte verwenden.',
+    contact: 'Schicken Sie uns gerne Kontaktdaten. Wir nutzen die Kontaktdaten für unsere Recherchen und werden die Person eventuell kontaktieren, aber keine Kontaktdaten veröffentlichen.',
+    medicalInfo: 'Sie stellen uns hier möglicherweise sensible medizinische Informationen bereit. Wir sind uns dessen bewusst und behandeln Ihre Daten mit aller gebotenen Vorsicht. Wenn Sie uns Daten schicken, helfen uns diese Informationen für unsere weiteren Recherchen. Ihre Daten werden wir nur intern auswerten, aber nicht veröffentlichen. Es sei denn, Sie geben Ihr ausdrückliches Einverständnis, nachdem wir Sie separat darum gebeten haben.',
+    confidential: 'Sie stellen uns hier möglicherweise vertrauliche Informationen bereit. Wir sind uns dessen bewusst und behandeln diese Informationen mit aller gebotenen Vorsicht. Wir werden Ihre Informationen nur intern auswerten, aber nicht ungefragt veröffentlichen.'
   }.freeze
 
   def plaintext
     parts = []
-    parts << 'Hallo, die Redaktion hat eine neue Frage an dich:'
+    parts << 'Hallo, die Redaktion hat eine neue Frage an Sie:'
     parts << text
     parts += hints.map { |hint| HINT_TEXTS[hint.to_sym] }
-    parts << 'Vielen Dank für deine Hilfe bei unserer Recherche!'
+    parts << 'Vielen Dank für Ihre Hilfe bei unserer Recherche!'
     parts.join("\n\n")
   end
 
