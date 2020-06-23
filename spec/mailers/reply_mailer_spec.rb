86380d8d3e1e03f9419602d184b4cd658e968135
Till Prochaska <mail@tillprochaska.de>
2020-06-23 19:03:38 +0200

    Fix tests

--- spec/mailers/reply_mailer_spec.rb
+++ spec/mailers/reply_mailer_spec.rb
@@ -3,5 +3,17 @@
 require 'rails_helper'
 
 RSpec.describe ReplyMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'new_question_email' do
+    let(:mail) { described_class.with(email: 'user@example.org').user_not_found_email }
+
+    describe '#subject' do
+      subject { mail.subject }
+      it { should eq('Wir können deine E-Mail Adresse nicht zuordnen') }
+    end
+
+    describe '#body' do
+      subject { mail.body }
+      it { should eq('Wir können deine E-Mail Adresse user@example.org leider keinem unserer Benutzerprofile zuordnen.') }
+    end
+  end
 end
