ccf5957c890c288975df14586954453397aa7c1f
Till Prochaska <mail@tillprochaska.de>
2020-08-20 12:26:12 +0200

    Refactor request metrics, box and card components

--- app/components/request_metrics/request_metrics.rb
+++ app/components/request_metrics/request_metrics.rb
@@ -7,5 +7,33 @@ module RequestMetrics
 
       @request = request
     end
+
+    def call
+      render Metrics::Metrics.new(metrics: metrics, styles: styles)
+    end
+
+    private
+
+    attr_reader :request
+
+    def metrics
+      [
+        {
+          value: request.stats[:counts][:users],
+          label: I18n.t('components.request_metrics.users'),
+          icon: 'single-03'
+        },
+        {
+          value: request.stats[:counts][:replies],
+          label: I18n.t('components.request_metrics.replies'),
+          icon: 'a-chat'
+        },
+        {
+          value: request.stats[:counts][:photos],
+          label: I18n.t('components.request_metrics.photos'),
+          icon: 'camera'
+        }
+      ]
+    end
   end
 end
--- spec/components/request_metrics_spec.rb
+++ spec/components/request_metrics_spec.rb
@@ -5,6 +5,10 @@ require 'rails_helper'
 RSpec.describe RequestMetrics::RequestMetrics, type: :component do
   subject { render_inline(described_class.new(**params)) }
 
-  let(:params) { {} }
-  it { should have_css('.RequestMetrics') }
+  let(:params) { { request: request } }
+  let(:request) { build(:request) }
+
+  it { should have_text('haben geantwortet') }
+  it { should have_text('Nachrichten insgesamt') }
+  it { should have_text('Fotos') }
 end
