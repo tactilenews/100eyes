05d9c85e819f2d3e63a63ad7b87aa57fa972963b
mattwr18 <mattwr18@gmail.com>
2020-09-22 17:29:54 +0200

    Fix test since RequestNotification is hidden to start

--- spec/components/request_notification_spec.rb
+++ spec/components/request_notification_spec.rb
@@ -5,6 +5,8 @@ require 'rails_helper'
 RSpec.describe RequestNotification::RequestNotification, type: :component do
   subject { render_inline(described_class.new(**params)) }
 
-  let(:params) { {} }
-  it { should have_css('.RequestNotification') }
+  let(:params) { { request: request } }
+  let(:request) { build(:request) }
+
+  it { should have_css('.RequestNotification', visible: :hidden) }
 end
