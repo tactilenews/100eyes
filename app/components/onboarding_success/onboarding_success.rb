e00f31c147fd7ac20c5863d781687d0e4464d0bf
Till Prochaska <mail@tillprochaska.de>
2020-10-05 11:03:29 +0200

    Move translation logic out of component
    Signed-off-by: Till Prochaska <mail@tillprochaska.de>

--- app/components/onboarding_success/onboarding_success.rb
+++ app/components/onboarding_success/onboarding_success.rb
@@ -2,5 +2,15 @@
 
 module OnboardingSuccess
   class OnboardingSuccess < ApplicationComponent
+    def initialize(heading:, text:, **)
+      super
+
+      @heading = heading
+      @text = text
+    end
+
+    private
+
+    attr_reader :heading, :text
   end
 end
--- spec/components/onboarding_success_spec.rb
+++ spec/components/onboarding_success_spec.rb
@@ -5,6 +5,6 @@ require 'rails_helper'
 RSpec.describe OnboardingSuccess::OnboardingSuccess, type: :component do
   subject { render_inline(described_class.new(**params)) }
 
-  let(:params) { {} }
+  let(:params) { { heading: 'Hello', text: 'World' } }
   it { should have_css('.OnboardingSuccess') }
 end
