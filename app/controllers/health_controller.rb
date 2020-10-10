55b93c80ce3aa3a6bb7526dd1391470f83f47d7a
Till Prochaska <mail@tillprochaska.de>
2020-10-10 18:38:44 +0200

    Explicitly check database connection and pending migrations
    https://github.com/tactilenews/100eyes/pull/423#discussion_r502806753
    Signed-off-by: Till Prochaska <mail@tillprochaska.de>

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -4,6 +4,11 @@ class HealthController < ApplicationController
   skip_before_action :authenticate
 
   def index
+    ActiveRecord::Base.connection.execute('select 1')
+    ActiveRecord::Migration.check_pending!
+  rescue StandardError
+    render status: :service_unavailable, json: { status: 'Service Unavailable' }
+  else
     render json: { status: 'OK' }
   end
 end
