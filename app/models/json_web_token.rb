8ef0f29eab73702e3b6b2eeda3f29bef8c3abf74
mattwr18 <mattwr18@gmail.com>
2020-10-19 12:40:27 +0200

    Hard code expiration time, not configurable

--- app/models/json_web_token.rb
+++ app/models/json_web_token.rb
@@ -3,8 +3,8 @@
 class JsonWebToken < ApplicationRecord
   SECRET_KEY = Rails.application.secrets.secret_key_base.to_s
 
-  def self.encode(payload, exp = 48.hours.from_now.to_i)
-    exp_payload = { data: payload, exp: exp }
+  def self.encode(payload)
+    exp_payload = { data: payload, exp: 48.hours.from_now.to_i }
     JWT.encode(exp_payload, SECRET_KEY, 'HS256')
   end
 
