4e6171f96dce864f554e1cc821bc6f12f3b69874
mattwr18 <mattwr18@gmail.com>
2020-10-27 12:57:58 +0100

    Remove unused props from telegram widget

--- app/components/telegram_widget/telegram_widget.rb
+++ app/components/telegram_widget/telegram_widget.rb
@@ -2,14 +2,6 @@
 
 module TelegramWidget
   class TelegramWidget < ApplicationComponent
-    def initialize(stimulus_target: nil, **)
-      super
-
-      @stimulus_target = stimulus_target
-    end
-
-    private
-
-    attr_reader :stimulus_target
+    def initialize(**); end
   end
 end
