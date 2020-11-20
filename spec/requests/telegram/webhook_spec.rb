246b3cebc8f28f7c2a03f4a9f94a9f0370b1c5fd
mattwr18 <mattwr18@gmail.com>
2020-11-20 19:04:24 +0100

    Remove tests after removing method

--- spec/requests/telegram/webhook_spec.rb
+++ spec/requests/telegram/webhook_spec.rb
@@ -4,23 +4,6 @@ require 'rails_helper'
 require 'telegram/bot/rspec/integration/rails'
 
 RSpec.describe Telegram::WebhookController, telegram_bot: :rails do
-  describe '#start!' do
-    before do
-      Setting.telegram_contributor_not_found_message = 'Who are you?'
-    end
-
-    context 'has onboarded' do
-      let(:contributor) { create(:contributor, telegram_id: 12_345) }
-      subject { -> { dispatch_command :start, { from: { id: contributor.telegram_id } } } }
-      it { should_not respond_with_message 'Who are you?' }
-    end
-
-    context 'contributor has not onboarded yet' do
-      subject { -> { dispatch_command :start, { from: { id: 'whoami' } } } }
-      it { should respond_with_message 'Who are you?' }
-    end
-  end
-
   describe '#message' do
     subject { -> { dispatch_message 'Hello Bot!', { from: { id: 'whoami' } } } }
 
