62425f1b59bae458eeff83c65af6392e134c4fa9
mattwr18 <mattwr18@gmail.com>
2020-11-20 18:48:02 +0100

    Remove start message

--- app/controllers/telegram/webhook_controller.rb
+++ app/controllers/telegram/webhook_controller.rb
@@ -10,12 +10,4 @@ class Telegram::WebhookController < Telegram::Bot::UpdatesController
     respond_with :message, text: Setting.telegram_unknown_content_message if telegram_message.unknown_content
     contributor.reply(telegram_message)
   end
-
-  def start!(_data = nil, *)
-    telegram_message = TelegramMessage.new(from: from, chat: chat)
-    contributor = telegram_message.sender
-    return if contributor
-
-    respond_with :message, text: Setting.telegram_contributor_not_found_message
-  end
 end
