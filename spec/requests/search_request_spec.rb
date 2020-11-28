b18d88aa6c40c936d8b8b7a19f2b5ceb43845d81
mattwr18 <mattwr18@gmail.com>
2020-11-28 16:17:30 +0100

    Extend auth logic to remaining specs

--- spec/requests/search_request_spec.rb
+++ spec/requests/search_request_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.describe 'Searches', type: :request do
   describe 'GET /index' do
     it 'returns http success' do
-      get '/search'
+      get search_path(as: create(:user))
       expect(response).to have_http_status(:success)
     end
   end
