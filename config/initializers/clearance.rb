4a10ec8a2a52f2368abe286f9da1661498ce2840
mattwr18 <mattwr18@gmail.com>
2020-11-28 16:52:12 +0100

    Add some basic configuration

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 Clearance.configure do |config|
-  config.mailer_sender = 'reply@example.com'
+  config.allow_sign_up = false
+  config.cookie_domain = lambda { |request| request.host }
+  config.cookie_expiration = lambda { |cookies| 1.day.from_now }
+  config.secure_cookie = true
+  # config.sign_in_guards = []
+  config.mailer_sender = Setting.email_from_address
   config.rotate_csrf_on_sign_in = true
 end
