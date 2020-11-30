01dd27c18ebea2635d4f2256ba527190c30a11c2
mattwr18 <mattwr18@gmail.com>
2020-11-30 16:35:56 +0100

    Remove unused code

--- app/components/sign_in_form/sign_in_form.rb
+++ app/components/sign_in_form/sign_in_form.rb
@@ -1,15 +1,5 @@
 # frozen_string_literal: true
 
 module SignInForm
-  class SignInForm < ApplicationComponent
-    def initialize
-      super
-
-      @resource = User.new
-    end
-
-    private
-
-    attr_reader :resource
-  end
+  class SignInForm < ApplicationComponent; end
 end
