0888b425e3b4911b323e644875609c80354bd893
mattwr18 <mattwr18@gmail.com>
2020-11-30 19:49:52 +0100

    Add custom form for password reset with hints and password_confirmation

--- app/components/password_reset_form/password_reset_form.rb
+++ app/components/password_reset_form/password_reset_form.rb
@@ -1,5 +1,21 @@
 # frozen_string_literal: true
 
 module PasswordResetForm
-  class PasswordResetForm < ApplicationComponent; end
+  class PasswordResetForm < ApplicationComponent
+    def initialize(user: nil)
+      super
+
+      @user = user
+    end
+
+    private
+
+    attr_reader :user
+
+    def help
+      # rubocop:disable Rails/OutputSafety
+      I18n.t('helpers.hint.password_reset.password')&.html_safe
+      # rubocop:enable Rails/OutputSafety
+    end
+  end
 end
