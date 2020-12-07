2b68dd8b4ba708467ba28ec08d3b4d52f07fb7fb
mattwr18 <mattwr18@gmail.com>
2020-12-07 09:01:03 +0100

    Use signed jwt between authorization steps

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@
 
 class SessionsController < Clearance::SessionsController
   before_action :verify_user_params, only: :verify_user
-  before_action :set_user, only: :verify_user
+  before_action :verify_sign_in_jwt, only: :verify_user
   skip_before_action :require_login, only: %i[create new destroy verify_user], raise: false
 
   def create
@@ -10,14 +10,19 @@ class SessionsController < Clearance::SessionsController
 
     return unless @user
 
+    payload = { user_id: @user.id, action: 'sign_in' }
+    @jwt = JsonWebToken.encode(payload, expires_in: 3.minutes.from_now.to_i)
     @qr_code = RQRCode::QRCode.new(@user.provisioning_uri(Setting.project_name), size: 7, level: :h)
     render 'sessions/two_factor_authentication'
   end
 
   def verify_user
+    decoded_token = JsonWebToken.decode(jwt_param)
+    user_id = decoded_token.first['data']['user_id']
+    @user = User.find(user_id)
     return unless @user
 
-    authenticate_otp = @user&.authenticate_otp(verify_user_params['otp_code_token'], drift: 60)
+    authenticate_otp = @user.authenticate_otp(verify_user_params['otp_code_token'], drift: 60)
     return unless authenticate_otp
 
     @user.otp_module_enabled! if @user.otp_module_disabled?
@@ -34,11 +39,21 @@ class SessionsController < Clearance::SessionsController
 
   private
 
-  def verify_user_params
-    params.require(:session).permit(:otp_code_token, :user_id)
+  def verify_sign_in_jwt
+    decoded_token = JsonWebToken.decode(jwt_param)
+
+    if decoded_token.first['data']['action'] != 'sign_in' || decoded_token.first['data']['user_id'].blank?
+      raise ActionController::BadRequest
+    end
+  rescue StandardError
+    render 'onboarding/unauthorized', status: :unauthorized
   end
 
-  def set_user
-    @user = User.find(params[:user_id])
+  def jwt_param
+    params.require(:jwt)
+  end
+
+  def verify_user_params
+    params.require(:session).permit(:otp_code_token)
   end
 end
