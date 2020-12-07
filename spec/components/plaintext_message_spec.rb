01a2aed9054627e31fd94e6c5fca4ca17b13d411
Till Prochaska <mail@tillprochaska.de>
2020-12-07 14:20:20 +0100

    Fix #565
    Signed-off-by: Till Prochaska <mail@tillprochaska.de>

--- spec/components/plaintext_message_spec.rb
+++ spec/components/plaintext_message_spec.rb
@@ -19,7 +19,23 @@ RSpec.describe PlaintextMessage::PlaintextMessage, type: :component do
     end
 
     it 'strips whitespace' do
-      expect(subject.child.inner_html).to eq('Hello World!')
+      expect(subject).to have_css('p', count: 1, exact_text: 'Hello World!')
+      # expect(subject.find('p').first.inner_html).to eq('Hello World!')
+    end
+  end
+
+  describe 'given a message with consecutive line breaks' do
+    let(:message) do
+      <<~MESSAGE
+        This message contains consecutive
+
+
+        line breaks
+      MESSAGE
+    end
+
+    it 'renders two p tags' do
+      expect(subject).to have_css('p', count: 2)
     end
   end
 
@@ -27,7 +43,6 @@ RSpec.describe PlaintextMessage::PlaintextMessage, type: :component do
     let(:message) { nil }
 
     it 'renders successfully' do
-      expect(subject).to have_css('.PlaintextMessage')
       expect(subject.child.inner_html).to be_empty
     end
   end
