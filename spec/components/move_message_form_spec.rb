b8e49d23f996012b162e60c7669f008366bcc214
Till Prochaska <mail@tillprochaska.de>
2021-01-17 23:31:58 +0100

    Add component spec
    Signed-off-by: Till Prochaska <mail@tillprochaska.de>

--- spec/components/move_message_form_spec.rb
+++ spec/components/move_message_form_spec.rb
@@ -5,8 +5,33 @@ require 'rails_helper'
 RSpec.describe MoveMessageForm::MoveMessageForm, type: :component do
   subject { render_inline(described_class.new(**params)) }
 
-  let(:message) { create(:message) }
+  let!(:older_request) { create(:request, created_at: 1.hour.ago) }
+  let!(:current_request) { create(:request, created_at: 0.hours.ago) }
+  let!(:newer_request) { create(:request, created_at: 1.hour.from_now) }
+
+  let(:message) { create(:message, request: current_request) }
+
   let(:params) { { message: message } }
 
   it { should have_css('form') }
+  it { should have_css('input[type="radio"]', count: 2) }
+
+  it 'displays current request checked' do
+    first = subject.css('input[type="radio"]').first
+
+    expect(first[:value]).to eq(current_request.id.to_s)
+    expect(first[:checked]).to eq('checked')
+  end
+
+  it 'displays older request unchecked' do
+    last = subject.css('input[type="radio"]').last
+
+    expect(last[:value]).to eq(older_request.id.to_s)
+    expect(last[:checked]).to be_nil
+  end
+
+  it 'does not show newer request' do
+    request_ids = subject.css('input[type="radio"]').pluck(:value)
+    expect(request_ids).not_to include(newer_request.id)
+  end
 end
