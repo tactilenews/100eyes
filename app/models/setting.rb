dec315259ef64cfc5315919726e490dcab35bf3e
roschaefer <git@roschaefer.de>
2021-01-18 17:15:08 +0100

    fix: no method `onboarding_logo` on Setting
    @tillprochaska it seems the gem does not add methods to the settings
    object if the field would have default value `nil`.

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -7,8 +7,8 @@ class Setting < RailsSettings::Base
   field :project_name, default: ENV['HUNDRED_EYES_PROJECT_NAME'] || '100eyes'
   field :application_host, readonly: true, default: ENV['APPLICATION_HOSTNAME'] || 'localhost:3000'
 
-  field :onboarding_logo
-  field :onboarding_hero
+  field :onboarding_logo, default: ''
+  field :onboarding_hero, default: ''
   field :onboarding_title, default: 'Hallo und herzlich willkommen!'
   field :onboarding_page, default: File.read(File.join('config', 'locales', 'onboarding', 'page.md'))
   field :onboarding_success_heading, default: File.read(File.join('config', 'locales', 'onboarding', 'success_heading.txt'))
