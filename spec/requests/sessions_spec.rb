2b9a960312fe5debd60d7cbef85ca8d8701cde8d
mattwr18 <mattwr18@gmail.com>
2021-01-19 17:33:38 +0100

    Update sessions request specs with new workflow

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -3,24 +3,25 @@
 require 'rails_helper'
 
 RSpec.describe 'Sessions' do
-  describe 'POST /session/verify_user_email_and_password' do
-    subject { post '/session/verify_user_email_and_password', params: params }
+  describe 'POST /session' do
+    subject { post '/session', params: params }
 
     let(:params) do
-      { session: { email: email, password: password } }
+      { session: { email: email, password: password, otp_code: otp_code } }
     end
+    let(:email) { Faker::Internet.safe_email }
+    let(:password) { Faker::Internet.password(min_length: 20, max_length: 128) }
+    let(:otp_code) { nil }
 
     context "User doesn't exist" do
-      let(:email) { Faker::Internet.safe_email }
-      let(:password) { Faker::Internet.password(min_length: 20, max_length: 128) }
       before { subject }
 
-      it 'Redirects' do
-        expect(response).to redirect_to('/sign_in')
+      it 'is unauthorized' do
+        expect(response).to be_unauthorized
       end
 
-      it 'displays error message' do
-        expect(response.request.flash[:error]).to eq(I18n.t('flashes.failure_after_create'))
+      it 'displays error message, but does not give off if a user with email exists' do
+        expect(response.request.flash[:alert]).to eq(I18n.t('flashes.failure_after_create'))
       end
     end
 
@@ -35,12 +36,12 @@ RSpec.describe 'Sessions' do
 
         before { subject }
 
-        it 'Redirects' do
-          expect(response).to redirect_to('/sign_in')
+        it 'is unauthorized' do
+          expect(response).to be_unauthorized
         end
 
-        it 'displays error message' do
-          expect(response.request.flash[:error]).to eq(I18n.t('flashes.failure_after_create'))
+        it 'displays error message, but does not give off if a user with email exists' do
+          expect(response.request.flash[:alert]).to eq(I18n.t('flashes.failure_after_create'))
         end
       end
 
@@ -50,36 +51,23 @@ RSpec.describe 'Sessions' do
 
         before { subject }
 
-        it 'Redirects' do
-          expect(response).to redirect_to('/sign_in')
+        it 'is unauthorized' do
+          expect(response).to be_unauthorized
         end
 
-        it 'displays error message' do
-          expect(response.request.flash[:error]).to eq(I18n.t('flashes.failure_after_create'))
+        it 'displays error message, but does not give off if a user with email exists' do
+          expect(response.request.flash[:alert]).to eq(I18n.t('flashes.failure_after_create'))
         end
       end
 
       context 'Correct email/password combination' do
-        let(:cookie_jar) { ActionDispatch::Cookies::CookieJar.build(request, cookies.to_hash) }
         let(:email) { valid_email }
         let(:password) { valid_password }
-
-        context 'otp_enabled? true' do
-          before { subject }
-
-          it 'Is successful' do
-            expect(response).to be_successful
-          end
-
-          it 'Sets encrypted cookie to store user_id' do
-            expect(cookie_jar.encrypted['sessions_user_id']).to eq(user.id)
-          end
-        end
 
         context 'otp_enabled? false' do
           before { user.update(otp_enabled: false) }
 
-          it 'redirects to user settings to set up 2fa' do
+          it 'allows signing in without code, but redirects to user settings to set up 2fa' do
             subject
             expect(response).to redirect_to(dashboard_path)
 
@@ -87,54 +75,14 @@ RSpec.describe 'Sessions' do
             expect(response).to redirect_to(two_factor_auth_setup_user_setting_path(user))
           end
         end
-      end
-    end
-  end
 
-  describe 'PATCH /session/verify_user_otp' do
-    subject { patch '/session/verify_user_otp', params: { session: { otp_code: otp_code } } }
+        context 'otp_enabled? true' do
+          before { subject }
+          let(:otp_code) { user.otp_code }
 
-    let(:otp_code) { SecureRandom.hex(3) }
-
-    context 'No user' do
-      it 'returns status unauthorized, but does not reveal whether a user exists or not' do
-        subject
-        expect(response).to be_unauthorized
-      end
-    end
-
-    context 'User exists' do
-      let(:user) { create(:user) }
-
-      before do
-        my_cookies = ActionDispatch::Request.new(Rails.application.env_config.deep_dup).cookie_jar
-        my_cookies.encrypted[:sessions_user_id] = { value: user.id, expires: 3.minutes }
-        cookies[:sessions_user_id] = my_cookies[:sessions_user_id]
-      end
-
-      context 'Unauthorized' do
-        before { subject }
-
-        it 'Invalid otp_code' do
-          expect(response).to be_unauthorized
-        end
-
-        it 'displays error message' do
-          expect(response.request.flash[:error]).to eq(I18n.t('components.two_factor_authentication.failure_message'))
-        end
-      end
-
-      context 'Authorized' do
-        let(:otp_code) { user.otp_code }
-
-        it 'valid otp_code' do
-          subject
-          expect(response).to redirect_to('/dashboard')
-        end
-
-        it 'sets remember_token for user' do
-          subject
-          expect(response.cookies['remember_token']).to eq(user.remember_token)
+          it 'redirects to the dashboard' do
+            expect(response).to redirect_to(dashboard_path)
+          end
         end
       end
     end
