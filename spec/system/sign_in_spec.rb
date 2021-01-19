61b8124825a3796a8c59899fd9bc38b3f09b33a0
mattwr18 <mattwr18@gmail.com>
2021-01-19 18:11:24 +0100

    Fix sign in system spec, update translations setup form

--- spec/system/sign_in_spec.rb
+++ spec/system/sign_in_spec.rb
@@ -4,26 +4,56 @@ require 'rails_helper'
 
 RSpec.describe 'Sign in' do
   context 'Valid' do
-    let(:user) { create(:user, email: email, password: password) }
     let(:email) { Faker::Internet.safe_email }
     let(:password) { Faker::Internet.password(min_length: 20, max_length: 128) }
 
-    it 'Redirects to dashboard on successful login' do
-      visit '/dashboard'
+    context 'otp_enabled? false' do
+      let(:user) { create(:user, email: email, password: password, otp_enabled: false) }
 
-      expect(page).to have_current_path('/sign_in')
+      it 'Redirects to user setting page to enable 2fa' do
+        visit dashboard_path
 
-      fill_in I18n.t('helpers.label.password.email'), with: user.email
-      fill_in I18n.t('helpers.label.session.password'), with: user.password
-      click_button I18n.t('helpers.submit.session.submit')
+        expect(page).to have_current_path('/sign_in')
 
-      expect(page).to have_current_path('/session/verify_user_email_and_password')
+        fill_in I18n.t('helpers.label.password.email'), with: user.email
+        fill_in I18n.t('helpers.label.session.password'), with: user.password
+        click_button I18n.t('helpers.submit.session.submit')
 
-      fill_in I18n.t('components.two_factor_authentication.label'), with: user.otp_code
-      click_button I18n.t('components.two_factor_authentication.submit')
+        expect(page).to have_current_path(two_factor_auth_setup_user_setting_path(user))
 
-      expect(page).to have_current_path('/dashboard')
-      expect(page).to have_button(I18n.t('components.nav_bar.sign_out'))
+        # Does not allow to use site without enabling otp
+        click_link I18n.t('setting.setting.other')
+        expect(page).to have_current_path(two_factor_auth_setup_user_setting_path(user))
+
+        click_link I18n.t('application_name')
+        expect(page).to have_current_path(two_factor_auth_setup_user_setting_path(user))
+        expect(page).to have_css('svg')
+
+        fill_in I18n.t('components.two_factor_auth_setup.otp_code.label'), with: user.otp_code
+        click_button I18n.t('components.two_factor_auth_setup.otp_code.submit')
+
+        expect(page).to have_current_path(dashboard_path)
+        expect(page).to have_link(I18n.t('components.nav_bar.sign_out'))
+      end
+    end
+
+    context 'otp_enabled? true' do
+      let(:user) { create(:user, email: email, password: password) }
+
+      it 'Redirects to dashboard on successful sign in' do
+        visit dashboard_path
+
+        expect(page).to have_current_path('/sign_in')
+
+        fill_in I18n.t('helpers.label.password.email'), with: user.email
+        fill_in I18n.t('helpers.label.session.password'), with: user.password
+        fill_in I18n.t('components.sign_in_form.otp_code.label'), with: user.otp_code
+
+        click_button I18n.t('helpers.submit.session.submit')
+
+        expect(page).to have_current_path(dashboard_path)
+        expect(page).to have_link(I18n.t('components.nav_bar.sign_out'))
+      end
     end
   end
 end
