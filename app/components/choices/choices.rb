a185cdc05c3622478e46545924616f78f3c3c869
mattwr18 <mattwr18@gmail.com>
2021-01-25 16:39:57 +0100

    Update choices to use new target syntax

--- app/components/choices/choices.rb
+++ app/components/choices/choices.rb
@@ -2,15 +2,16 @@
 
 module Choices
   class Choices < ApplicationComponent
-    def initialize(choices: [], stimulus_target: nil, **)
+    def initialize(choices: [], stimulus_controller: nil, stimulus_target: nil, **)
       super
 
       @choices = choices
+      @stimulus_controller = stimulus_controller
       @stimulus_target = stimulus_target
     end
 
     private
 
-    attr_reader :choices, :stimulus_target
+    attr_reader :choices, :stimulus_target, :stimulus_controller
   end
 end
