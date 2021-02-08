8f859f19835309c6d8897af0eb2555113eda28a8
mattwr18 <mattwr18@gmail.com>
2021-02-08 20:56:02 +0100

    Add adapter spec for three adapter

---
+++ spec/adapters/threema_adapter_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ThreemaAdapter do
+  let(:adapter) { described_class.new(message: message) }
+  let(:threema_mock) { double('Threema', send: 'I was sent successfully!') }
+  let(:threema) { instance_double(Threema) }
+  let(:contributor) { create(:contributor, threema_id: 'V5EA564T') }
+  let(:message) { build(:message, recipient: contributor) }
+
+  before do
+    allow(Threema).to receive(:new).and_return(threema)
+  end
+
+  describe '#send!' do
+    subject { adapter.send! }
+
+    it 'sends the message' do
+      expect(threema).to receive(:send).with({ type: :text, threema_id: contributor.threema_id, text: message.text })
+
+      subject
+    end
+  end
+end
