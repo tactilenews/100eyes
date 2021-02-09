566ab6f927e613ecca755093767e99feb6793e3a
mattwr18 <mattwr18@gmail.com>
2021-02-09 22:31:23 +0100

    Handle unknown_content
    - Rescue NameError returned from Threema::Receive::DeliveryReceipt not
      being declared in threema gem
    - respond to contributors trying to send files/images with message...
      return 200 to avoid Threema Gateway API retry logic

--- app/controllers/threema/webhook_controller.rb
+++ app/controllers/threema/webhook_controller.rb
@@ -3,8 +3,7 @@
 require 'openssl'
 
 class Threema::WebhookController < ApplicationController
-  protect_from_forgery with: :null_session
-  skip_before_action :require_login
+  skip_before_action :require_login, :verify_authenticity_token
 
   def message
     threema_message = ThreemaMessage.new(threema_webhook_params)
@@ -12,14 +11,26 @@ class Threema::WebhookController < ApplicationController
 
     return unless contributor
 
+    if threema_message.unknown_content
+      respond_to_unknown_content_and_prevent_retries(contributor)
+      return
+    end
+
     head :ok if contributor.reply(threema_message)
   rescue ActiveRecord::RecordInvalid
     head :service_unavailable
+  rescue NameError
+    respond_to_unknown_content_and_prevent_retries(contributor)
   end
 
   private
 
   def threema_webhook_params
     params.permit(:from, :to, :messageId, :date, :nonce, :box, :mac, :nickname)
+  end
+
+  def respond_to_unknown_content_and_prevent_retries(contributor)
+    ThreemaAdapter.new(message: Message.new(text: Setting.telegram_unknown_content_message, recipient: contributor)).send!
+    head :ok
   end
 end
--- app/decorators/threema_message.rb
+++ app/decorators/threema_message.rb
@@ -1,22 +1,29 @@
 # frozen_string_literal: true
 
 class ThreemaMessage
-  attr_reader :sender, :message
+  UNKNOWN_CONTENT_CLASS = [Threema::Receive::File, Threema::Receive::Image].freeze
+
+  attr_reader :sender, :unknown_content, :message
 
   def initialize(threema_message)
-    threema_decrypted = Threema.new.receive(payload: threema_message)
+    decrypted_message = Threema.new.receive(payload: threema_message)
     @sender = Contributor.find_by(threema_id: threema_message[:from])
     return unless @sender
 
-    @message = initialize_message(threema_decrypted)
+    @unknown_content = initialize_unknown_content(decrypted_message)
+    @message = initialize_message(decrypted_message)
   end
 
   private
 
-  def initialize_message(threema_decrypted)
-    message = Message.new(text: threema_decrypted.content, sender: sender)
+  def initialize_unknown_content(decrypted_message)
+    @unknown_content = UNKNOWN_CONTENT_CLASS.any? { |klass| decrypted_message.is_a? klass }
+  end
+
+  def initialize_message(decrypted_message)
+    message = Message.new(text: decrypted_message.content, sender: sender)
     message.raw_data.attach(
-      io: StringIO.new(JSON.generate(threema_decrypted)),
+      io: StringIO.new(JSON.generate(decrypted_message)),
       filename: 'threema_api.json',
       content_type: 'application/json'
     )
