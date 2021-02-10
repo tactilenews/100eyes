f41623cbfaaa36e104a5788de59de45db618eb5f
mattwr18 <mattwr18@gmail.com>
2021-02-10 20:20:18 +0100

    Get basic receiving voice messages working

--- app/adapters/threema_adapter/inbound.rb
+++ app/adapters/threema_adapter/inbound.rb
@@ -2,9 +2,9 @@
 
 module ThreemaAdapter
   class Inbound
-    UNKNOWN_CONTENT_CLASS = [Threema::Receive::File, Threema::Receive::Image].freeze
+    UNKNOWN_CONTENT_CLASS = [Threema::Receive::Image].freeze
 
-    attr_reader :sender, :unknown_content, :message, :delivery_receipt
+    attr_reader :sender, :text, :unknown_content, :message, :delivery_receipt
 
     def initialize(threema_message)
       decrypted_message = Threema.new.receive(payload: threema_message)
@@ -13,8 +13,11 @@ module ThreemaAdapter
       return unless @sender
 
       @delivery_receipt = delivery_receipt?(decrypted_message)
+      @text = initialize_text(decrypted_message)
       @unknown_content = initialize_unknown_content(decrypted_message)
       @message = initialize_message(decrypted_message)
+      @voice = initialize_voice(decrypted_message)
+      @message.voice = @voice
     end
 
     private
@@ -23,18 +26,43 @@ module ThreemaAdapter
       decrypted_message.instance_of? Threema::Receive::DeliveryReceipt
     end
 
+    def initialize_text(decrypted_message)
+      return nil unless decrypted_message.is_a? Threema::Receive::Text
+  
+      decrypted_message.content
+    end
+
     def initialize_unknown_content(decrypted_message)
       @unknown_content = UNKNOWN_CONTENT_CLASS.any? { |klass| decrypted_message.instance_of? klass }
     end
 
     def initialize_message(decrypted_message)
-      message = Message.new(text: decrypted_message.content, sender: sender)
+      message = Message.new(text: text, sender: sender)
       message.raw_data.attach(
         io: StringIO.new(decrypted_message.content),
         filename: 'threema_api.json',
         content_type: 'application/json'
       )
+      message.unknown_content = unknown_content
       message
+    end
+
+    def initialize_voice(decrypted_message)
+      return nil unless decrypted_message.respond_to? :mime_type
+  
+      if decrypted_message.mime_type != 'audio/aac'
+        message.unknown_content = true
+        return
+      end
+  
+      voice = Voice.new
+      voice.attachment.attach(
+        io: StringIO.new(decrypted_message.content),
+        filename: decrypted_message.name,
+        content_type: :audio,
+        identify: false
+      )
+      voice
     end
   end
 end
