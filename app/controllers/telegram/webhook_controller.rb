731a2b3d2ec00ea8c7f7cffd29203061b6b93f7f
mattwr18 <mattwr18@gmail.com>
2021-02-11 21:43:21 +0100

    Ignore onboarding webhooks from telegram
    - the connected_website field is only used when a user logs in on the
      bot's connected website using the Login Widget and allows sending
    messages from your bot. Source https://core.telegram.org/bots/api-changelog#february-13-2018

--- app/controllers/telegram/webhook_controller.rb
+++ app/controllers/telegram/webhook_controller.rb
@@ -2,6 +2,8 @@
 
 class Telegram::WebhookController < Telegram::Bot::UpdatesController
   def message(message)
+    return if contributor_onboarding?(message)
+
     telegram_message = TelegramAdapter::Inbound.new(message)
     contributor = telegram_message.sender
 
@@ -9,5 +11,11 @@ class Telegram::WebhookController < Telegram::Bot::UpdatesController
 
     respond_with :message, text: Setting.telegram_unknown_content_message if telegram_message.unknown_content
     contributor.reply(telegram_message)
+  end
+
+  private
+
+  def contributor_onboarding?(message)
+    message['connected_website'] && message['connected_website'] == Setting.application_host
   end
 end
--- spec/requests/telegram/webhook_spec.rb
+++ spec/requests/telegram/webhook_spec.rb
@@ -5,7 +5,17 @@ require 'telegram/bot/rspec/integration/rails'
 
 RSpec.describe Telegram::WebhookController, telegram_bot: :rails do
   describe '#message' do
-    subject { -> { dispatch_message 'Hello Bot!', { from: { id: 'whoami' } } } }
+    context 'Unknown contributor' do
+      subject { -> { dispatch_message 'Hello Bot!', { from: { id: 'whoami' } } } }
+
+      it { is_expected.to respond_with_message Setting.telegram_contributor_not_found_message }
+
+      describe 'Message received confirming onboarding' do
+        subject { -> { dispatch_message 'Hello Bot!', { from: { id: 'whoami' }, connected_website: Setting.application_host } } }
+
+        it { is_expected.not_to respond_with_message Setting.telegram_contributor_not_found_message }
+      end
+    end
 
     context 'sending a message with a document' do
       before { Setting.telegram_unknown_content_message = "Cannot handle this, I'm sorry :(" }
