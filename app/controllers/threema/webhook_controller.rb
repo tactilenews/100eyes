f11a797db590d3fc4572195d8e0f154b526cd8e6
mattwr18 <mattwr18@gmail.com>
2021-02-11 14:47:19 +0100

    Still save the raw_data if a contributor sends unknown_content
    - to be able to access the message retrospectively
    - to keep consistent across the code base

--- app/controllers/threema/webhook_controller.rb
+++ app/controllers/threema/webhook_controller.rb
@@ -16,10 +16,7 @@ class Threema::WebhookController < ApplicationController
     # a message and 200 to avoid retries?
     return unless contributor
 
-    if threema_message.unknown_content
-      respond_to_unknown_content_and_prevent_retries(contributor)
-      return
-    end
+    respond_to_unknown_content(contributor) if threema_message.unknown_content
 
     head :ok if contributor.reply(threema_message)
   rescue ActiveRecord::RecordInvalid
@@ -32,8 +29,7 @@ class Threema::WebhookController < ApplicationController
     params.permit(:from, :to, :messageId, :date, :nonce, :box, :mac, :nickname)
   end
 
-  def respond_to_unknown_content_and_prevent_retries(contributor)
+  def respond_to_unknown_content(contributor)
     ThreemaAdapter.new(message: Message.new(text: Setting.telegram_unknown_content_message, recipient: contributor)).send!
-    head :ok
   end
 end
