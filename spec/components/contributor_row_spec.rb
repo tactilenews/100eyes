3bcf29a3c1a508a62ac0272c9349ac3d81b77369
phoeinx <hradilak@uni-potsdam.de>
2021-03-01 17:38:18 +0100

    Remove unneeded icons from icons.svg, update contributor_row tests

--- spec/components/contributor_row_spec.rb
+++ spec/components/contributor_row_spec.rb
@@ -8,13 +8,15 @@ RSpec.describe ContributorRow::ContributorRow, type: :component do
   let(:params) { { contributor: contributor } }
 
   it { should have_css('.ContributorRow') }
+  it { should have_text(contributor.name) }
 
   context 'when contributor uses one channel' do
+    let(:contributor) { create(:contributor, email: 'mail@mail.com') }
     it { should have_text('via Email') }
   end
 
   context 'when contributor uses multiple channels' do
-    let(:contributor) { create(:contributor, telegram_id: 42) }
+    let(:contributor) { create(:contributor, email: 'mail@mail.com', telegram_id: 42) }
     it { should have_text('via Email, Telegram') }
   end
 end
