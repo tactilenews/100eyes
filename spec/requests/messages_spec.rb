1b1a07d011c2bd916e7232e39536bbd5629c608b
phoeinx <hradilak@uni-potsdam.de>
2021-03-14 14:00:20 +0100

    feat: enable users to update manually created messages

--- spec/requests/messages_spec.rb
+++ spec/requests/messages_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe '/messages', type: :request do
   let(:contributor) { create(:contributor) }
   let(:request) { create(:request) }
   let(:user) { create(:user) }
+  let(:message) { create(:message) }
 
   describe 'GET /new' do
     it 'should be successful' do
@@ -32,6 +33,43 @@ RSpec.describe '/messages', type: :request do
 
     it 'saves current user as creator' do
       should change { Message.pluck(:creator_id) }.from([]).to([user.id])
+    end
+  end
+
+  describe 'PATCH /message/:id' do
+    let(:message) { create(:message, creator_id: user.id) }
+    let(:new_attrs) { { text: 'Grab your coat and get your hat' } }
+    subject { -> { patch message_url(message, as: user), params: { message: new_attrs } } }
+
+    it 'updates the requested message' do
+      subject.call
+      message.reload
+
+      expect(message.text).to eq('Grab your coat and get your hat')
+    end
+
+    it 'shows success notification' do
+      subject.call
+      expect(flash[:success]).not_to be_empty
+    end
+
+    it 'redirects to the conversation link the message belongs to' do
+      subject.call
+      expect(response).to redirect_to(contributor_request_path(id: message.request_id, contributor_id: message.sender_id))
+    end
+
+    context 'not manually created message' do
+      let(:message) { create(:message, creator_id: nil) }
+
+      it 'does not update the requested message' do
+        subject.call
+        expect(response).not_to be_successful
+      end
+
+      it 'shows error notification' do
+        subject.call
+        expect(flash[:error]).not_to be_empty
+      end
     end
   end
 end
