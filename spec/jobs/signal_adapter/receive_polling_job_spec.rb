7d1d8fbc7fc65992f12c5d673a0bd28d0a98d17a
Robert Schäfer <git@roschaefer.de>
2021-07-21 18:28:23 +0200

    test: add test for polling job

--- spec/jobs/signal_adapter/receive_polling_job_spec.rb
+++ spec/jobs/signal_adapter/receive_polling_job_spec.rb
@@ -3,5 +3,46 @@
 require 'rails_helper'
 
 RSpec.describe SignalAdapter::ReceivePollingJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#perform_later' do
+    subject { -> { described_class.perform_later } }
+    it 'enqueues a job' do
+      should have_enqueued_job
+    end
+  end
+  describe 'performing enqueued jobs', vcr: { cassette_name: :receive_signal_messages } do
+    subject do
+      lambda {
+        perform_enqueued_jobs do
+          described_class.perform_now
+        end
+      }
+    end
+
+    it 'does not crash' do
+      should_not raise_error
+    end
+
+    describe 'given a request' do
+      before { create(:request) }
+      it 'does not create messages' do
+        should_not(change { Message.count })
+      end
+
+      describe 'and a corresponding contributor' do
+        before do
+          create(:contributor, phone_number: '+4915112345789')
+          create(:contributor, phone_number: '+4915155555555')
+        end
+
+        it 'create a message' do
+          should(change { Message.count }.from(0).to(1))
+        end
+
+        it 'assigns the correct contributor' do
+          subject.call
+          expect(Message.first.contributor.phone_number).to eq('+4915112345789')
+        end
+      end
+    end
+  end
 end
