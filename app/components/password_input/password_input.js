db2567b99cf9b183e551f65054bd57f225ff3b40
Till Prochaska <mail@tillprochaska.de>
2021-09-01 13:51:50 +0200

    Do not show inline password validations
    We decided not to go for complex password requirements, i.e. the only requirement is a minimum length of 8 characters. This requirement can be expressed using native HTML input attributes, thus we do not need custom inline validations.

--- app/components/password_input/password_input.js
+++ app/components/password_input/password_input.js
@@ -1,23 +1,11 @@
 import { Controller } from 'stimulus';
 
 export default class extends Controller {
-  static targets = [
-    'input',
-    'toggle',
-    'minLength',
-    'letters',
-    'numbers',
-    'special',
-  ];
+  static targets = ['input', 'toggle'];
   static values = {
     unmask: String,
     mask: String,
-    minLength: Number,
   };
-
-  connect() {
-    this.validate();
-  }
 
   toggle() {
     this.inputTarget.type =
@@ -25,28 +13,5 @@ export default class extends Controller {
 
     this.toggleTarget.innerHTML =
       this.inputTarget.type === 'password' ? this.unmaskValue : this.maskValue;
-  }
-
-  validate() {
-    const minLength = this.minLengthValue;
-
-    const validators = {
-      minLength: value => value.length >= minLength,
-      numbers: value => value.match(/[0-9]/),
-      letters: value => value.match(/[a-zA-Z]/),
-      special: value => value.match(/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/),
-    };
-
-    const value = this.inputTarget.value;
-
-    Object.entries(validators).forEach(([key, validate]) => {
-      const state = validate(value) ? 'valid' : 'invalid';
-
-      if (!this.targets.find(key)) {
-        return;
-      }
-
-      this.targets.find(key).dataset.state = state;
-    });
   }
 }
--- app/components/password_input/password_input.rb
+++ app/components/password_input/password_input.rb
@@ -2,25 +2,15 @@
 
 module PasswordInput
   class PasswordInput < ApplicationComponent
-    def initialize(id:, minlength: 0, show_validations: false, **)
+    def initialize(id:, minlength: 0, **)
       super
 
       @id = id
       @minlength = minlength
-      @show_validations = show_validations
     end
 
     private
 
-    attr_reader :id, :minlength, :show_validations
-
-    def validations
-      {
-        minLength: t('.validations.minLength', length: minlength),
-        letters: t('.validations.letters'),
-        numbers: t('.validations.numbers'),
-        special: t('.validations.special')
-      }
-    end
+    attr_reader :id, :minlength
   end
 end
