a946e50eec957619a1ee7384de50f718c9c45509
Till Prochaska <mail@tillprochaska.de>
2021-09-03 20:39:58 +0200

    Update Signal onboarding flow
    We now ask for contact details first, then prompt the contributor to send us an initial message.

--- app/controllers/onboarding/signal_controller.rb
+++ app/controllers/onboarding/signal_controller.rb
@@ -2,12 +2,18 @@
 
 module Onboarding
   class SignalController < OnboardingController
+    skip_before_action :verify_jwt, only: :splash
+
     def splash; end
 
     private
 
     def attr_name
       :signal_phone_number
+    end
+
+    def redirect_to_success
+      redirect_to onboarding_signal_splash_path(jwt: nil)
     end
   end
 end
