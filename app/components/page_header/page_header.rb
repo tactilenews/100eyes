3bf0b78bfc8b8b6d638a049d1b609007c919d855
Till Prochaska <mail@tillprochaska.de>
2021-09-06 15:21:46 +0200

    Create new PageHeader component
    The page headers are slowly getting more complex, right now, they may contain one or more action buttons as well as a tab bar, so it makes sense to encapsulate the styling and logic into a separate component.

---
+++ app/components/page_header/page_header.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module PageHeader
+  class PageHeader < ApplicationComponent
+    renders_one :actions
+    renders_one :tab_bar
+
+    private
+
+    def styles
+      return super + [:tab_Bar] if tab_bar
+
+      super
+    end
+  end
+end
