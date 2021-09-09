6f4e3c22c5f983094f2b70127bd567a31f8c225b
Till Prochaska <mail@tillprochaska.de>
2021-09-09 15:03:57 +0200

    Adjust grouping of specs, reduce indentation

--- spec/jobs/signal_adapter/receive_polling_job_spec.rb
+++ spec/jobs/signal_adapter/receive_polling_job_spec.rb
@@ -27,6 +27,8 @@ RSpec.describe SignalAdapter::ReceivePollingJob, type: :job do
 
     describe 'given a registered signal phone number on the server', vcr: { cassette_name: :receive_signal_messages } do
       before do
+        create(:request)
+
         unless Setting.signal_server_phone_number
           allow(Setting).to receive(:signal_server_phone_number).and_return('SIGNAL_SERVER_PHONE_NUMBER')
         end
@@ -35,7 +37,7 @@ RSpec.describe SignalAdapter::ReceivePollingJob, type: :job do
         allow(Sentry).to receive(:capture_exception).with(an_instance_of(SignalAdapter::UnknownContributorError))
       end
 
-      describe 'if an unknown contributor sends us a message' do
+      describe 'given a message from an unknown contributor' do
         it { should_not(change { Message.count }) }
 
         it 'sends an error to Sentry so that our admins get notified' do
@@ -44,80 +46,68 @@ RSpec.describe SignalAdapter::ReceivePollingJob, type: :job do
         end
       end
 
-      describe 'given a request' do
-        before { create(:request) }
-
-        describe 'and one message from an unknown contributor' do
-          it { should_not(change { Message.count }) }
-        end
-
-        describe 'and one message from a known contributor' do
-          before do
-            create(:contributor, signal_phone_number: '+4915112345789')
-            create(:contributor, signal_phone_number: '+4915155555555')
-          end
-
-          it 'is expected to create a message' do
-            should(change { Message.count }.from(0).to(1))
-          end
-
-          it 'is expected to assign the correct contributor' do
-            subject.call
-            expect(Message.first.contributor.signal_phone_number).to eq('+4915112345789')
-          end
-        end
-
-        describe 'and multiple messages from known and unknown contributors', vcr: { cassette_name: :receive_multiple_signal_messages } do
-          before do
-            create(:contributor, signal_phone_number: '+4915112345789')
-          end
-
-          it 'creates a message for the known contributor' do
-            should(change { Message.count }.from(0).to(1))
-          end
-
-          it 'raises an error for the unknown contributor so that our admins get notified' do
-            subject.call
-            expect(Sentry).to have_received(:capture_exception)
-          end
-        end
-      end
-    end
-
-    describe 'on a message with attachments' do
-      let(:contributor) { create(:contributor, signal_phone_number: '+4915112345678') }
-      before do
-        unless Setting.signal_server_phone_number
-          allow(Setting).to receive(:signal_server_phone_number).and_return('SIGNAL_SERVER_PHONE_NUMBER')
-        end
-        allow(File).to receive(:open).and_call_original
-        allow(File).to receive(:open)
-          .with("#{Setting.signal_rest_cli_attachment_path}zuNhdpIHpRU_9Du-B4oG")
-          .and_return(file_fixture('signal_message_with_attachment').open)
-        create(:request)
-        contributor
-      end
-
-      describe 'if the attachment included in the message is supported',
-               vcr: { cassette_name: :receive_signal_messages_containing_supported_attachment } do
-        let(:attached_file) { Message.first.files.first.attachment }
-        it 'is expected to save the attachments as attached files' do
-          subject.call
-          expect(attached_file).to be_attached
-        end
-      end
-
-      describe 'if the attachment included in the message is not supported',
-               vcr: { cassette_name: :receive_signal_messages_containing_unsupported_attachment } do
+      describe 'given a message from a known contributor' do
         before do
-          allow(Setting).to receive(:signal_unknown_content_message).and_return('We cannot process this content')
+          create(:contributor, signal_phone_number: '+4915112345789')
+          create(:contributor, signal_phone_number: '+4915155555555')
         end
 
-        it 'bounces a warning to the contributor' do
-          should have_enqueued_job(SignalAdapter::Outbound).with(
-            text: 'We cannot process this content',
-            recipient: contributor
-          )
+        it 'is expected to create a message' do
+          should(change { Message.count }.from(0).to(1))
+        end
+
+        it 'is expected to assign the correct contributor' do
+          subject.call
+          expect(Message.first.contributor.signal_phone_number).to eq('+4915112345789')
+        end
+      end
+
+      describe 'given multiple messages from known and unknown contributors', vcr: { cassette_name: :receive_multiple_signal_messages } do
+        before do
+          create(:contributor, signal_phone_number: '+4915112345789')
+        end
+
+        it 'creates a message for the known contributor' do
+          should(change { Message.count }.from(0).to(1))
+        end
+
+        it 'raises an error for the unknown contributor so that our admins get notified' do
+          subject.call
+          expect(Sentry).to have_received(:capture_exception)
+        end
+      end
+
+      describe 'given a message with attachments' do
+        let!(:contributor) { create(:contributor, signal_phone_number: '+4915112345678') }
+
+        before do
+          allow(File).to receive(:open).and_call_original
+          allow(File).to receive(:open)
+            .with("#{Setting.signal_rest_cli_attachment_path}zuNhdpIHpRU_9Du-B4oG")
+            .and_return(file_fixture('signal_message_with_attachment').open)
+        end
+
+        describe 'that is supported',
+                 vcr: { cassette_name: :receive_signal_messages_containing_supported_attachment } do
+          let(:attached_file) { Message.first.files.first.attachment }
+          it 'is expected to save the attachments as attached files' do
+            subject.call
+            expect(attached_file).to be_attached
+          end
+        end
+
+        describe 'that is unsupported',
+                 vcr: { cassette_name: :receive_signal_messages_containing_unsupported_attachment } do
+          before do
+            allow(Setting).to receive(:signal_unknown_content_message).and_return('We cannot process this content')
+          end
+
+          it 'bounces a warning to the contributor' do
+            should have_enqueued_job(SignalAdapter::Outbound).with(
+              text: 'We cannot process this content',
+              recipient: contributor
+            )
+          end
         end
       end
     end
