85d46de49503051bb12eaaaf8c0b29ea12484f21
Till Prochaska <mail@tillprochaska.de>
2021-09-26 11:45:02 +0200

    Simplify requests feed component

--- app/components/requests_feed/requests_feed.rb
+++ app/components/requests_feed/requests_feed.rb
@@ -2,5 +2,18 @@
 
 module RequestsFeed
   class RequestsFeed < ApplicationComponent
+    def initialize(contributor:, **)
+      super
+
+      @contributor = contributor
+    end
+
+    private
+
+    attr_reader :contributor
+
+    def replies
+      @replies ||= contributor.recent_replies
+    end
   end
 end
