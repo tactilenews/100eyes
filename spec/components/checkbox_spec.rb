da1f19b6b63bce14948f27cee9b3dd2a74790b65
Till Prochaska <mail@tillprochaska.de>
2021-09-27 14:08:52 +0200

    Remove checkbox slider style, horizontal field style
    … as both aren’t in use anymore.

--- spec/components/checkbox_spec.rb
+++ spec/components/checkbox_spec.rb
@@ -6,12 +6,6 @@ RSpec.describe Checkbox::Checkbox, type: :component do
   subject { render_inline(described_class.new(**params)) }
 
   let(:params) { {} }
-  it { should have_css('.Checkbox') }
-  it { should_not have_css('.slider.round') }
 
-  describe 'styles: [:switch]' do
-    let(:params) { { styles: [:switch] } }
-    it { should have_css('.Checkbox.Checkbox--switch') }
-    it { should have_css('.Checkbox-slider') }
-  end
+  it { should have_css('input[type="checkbox"]') }
 end
