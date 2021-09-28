f3b04f287c4b745ad869699dd6051dbe3eb74272
Till Prochaska <mail@tillprochaska.de>
2021-09-28 14:31:12 +0200

    Display created_at, deactivated_at in contributor profiles

---
+++ spec/components/contributor_header_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ContributorHeader::ContributorHeader, type: :component do
+  subject { render_inline(described_class.new(**params)) }
+
+  let(:contributor) { create(:contributor, created_at: '2021-01-01') }
+  let(:params) { { contributor: contributor } }
+
+  it { should have_css('header') }
+  it { should have_css('h1', text: contributor.name) }
+  it { should have_css('p', text: 'Mitglied seit 01.01.2021') }
+
+  context 'given an inactive contributor' do
+    let(:contributor) { create(:contributor, created_at: '2021-01-01', deactivated_at: '2021-02-01') }
+    it { should have_css('p', text: 'Mitglied seit 01.01.2021') }
+    it { should have_css('p', text: 'deaktiviert am 01.02.2021') }
+  end
+end
