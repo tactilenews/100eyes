c980dbdb6bc0c03f542c9fe6e594c505b1c07ea6
Till Prochaska <mail@tillprochaska.de>
2021-11-03 10:59:51 +0100

    Use automatic event binding for `turbo:frame-render` envet

--- app/components/notifications/notifications.js
+++ app/components/notifications/notifications.js
@@ -1,24 +1,14 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-  connect() {
-    // When Turbo renders a frame it will replace only contents that are
-    // within that particular frame. In most cases, flash messages/notifications
-    // are rendered outside of frames. To work around this, we hook into the
-    // Turbo lifecycle and manually replace the current notifications container
-    // with the one from the new response.
-    this.frameRenderHandler = event =>
-      this.replaceNotifications(event.detail.fetchResponse);
-    window.addEventListener('turbo:frame-render', this.frameRenderHandler);
-  }
-
-  disconnect() {
-    window.removeEventListener('turbo:frame-render', this.frameRenderHandler);
-  }
-
-  async replaceNotifications(fetchResponse) {
+  // When Turbo renders a frame it will replace only contents that are
+  // within that particular frame. In most cases, flash messages/notifications
+  // are rendered outside of frames. To work around this, we hook into the
+  // Turbo lifecycle and manually replace the current notifications container
+  // with the one from the new response.
+  async replaceNotifications(event) {
     const newDocument = document.createElement('html');
-    newDocument.innerHTML = await fetchResponse.responseHTML;
+    newDocument.innerHTML = await event.detail.fetchResponse.responseHTML;
 
     const newNotifications = newDocument.querySelector(
       '[data-controller="notifications"]'
