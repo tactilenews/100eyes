05d1398b961073eb0c090f62b0459bba4de3d129
Till Prochaska <mail@tillprochaska.de>
2021-11-05 12:26:22 +0100

    Use server-side browser detection
    This is simpler + easier to test

--- spec/components/browser_support_notification_spec.rb
+++ spec/components/browser_support_notification_spec.rb
@@ -3,8 +3,26 @@
 require 'rails_helper'
 
 RSpec.describe BrowserSupportNotification::BrowserSupportNotification, type: :component do
-  subject { render_inline(described_class.new(**params)) }
+  subject { render_inline(described_class.new) }
+  let(:user_agent) { nil }
 
-  let(:params) { {} }
-  it { should have_css('.Notification', visible: :hidden) }
+  before(:each) { allow(request).to receive(:user_agent).and_return(user_agent) }
+
+  context 'if user agent is a modern browser' do
+    # Google Chrome / macOS
+    let(:user_agent) do
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 12_0_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'
+    end
+
+    it { should_not have_css('.BrowserSupportNotification') }
+  end
+
+  context 'if user agent is an unsupported browser' do
+    # Safari 12 / macOS
+    let(:user_agent) do
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 Safari/605.1.15'
+    end
+
+    it { should have_css('.BrowserSupportNotification') }
+  end
 end
