64156e22c15523abf2b5897e9b54bceaeaa1c3b3
phoeinx <hradilak@uni-potsdam.de>
2021-11-21 14:30:03 +0100

    Add browser support notification test for firefox as supported browser.

--- spec/components/browser_support_notification_spec.rb
+++ spec/components/browser_support_notification_spec.rb
@@ -9,20 +9,30 @@ RSpec.describe BrowserSupportNotification::BrowserSupportNotification, type: :co
   before(:each) { allow(request).to receive(:user_agent).and_return(user_agent) }
 
   context 'if user agent is a modern browser' do
-    # Google Chrome / macOS
-    let(:user_agent) do
-      'Mozilla/5.0 (Macintosh; Intel Mac OS X 12_0_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'
+    context 'on Google Chrome / macOS' do
+      let(:user_agent) do
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 12_0_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'
+      end
+
+      it { should_not have_css('.BrowserSupportNotification') }
     end
 
-    it { should_not have_css('.BrowserSupportNotification') }
+    context 'on Firefox / macOS' do
+      let(:user_agent) do
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:93.0) Gecko/20100101 Firefox/93.0'
+      end
+
+      it { should_not have_css('.BrowserSupportNotification') }
+    end
   end
 
   context 'if user agent is an unsupported browser' do
-    # Safari 12 / macOS
-    let(:user_agent) do
-      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 Safari/605.1.15'
-    end
+    context 'on Safari 12 / macOS' do
+      let(:user_agent) do
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 Safari/605.1.15'
+      end
 
-    it { should have_css('.BrowserSupportNotification') }
+      it { should have_css('.BrowserSupportNotification') }
+    end
   end
 end
