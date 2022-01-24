6b678bb617c005f235177df6890cec47dec3e692
Till Prochaska <mail@tillprochaska.de>
2022-01-24 11:56:49 +0100

    Link to admin dashboard in nav bar
    Close #1195

--- app/components/nav_bar/nav_bar.rb
+++ app/components/nav_bar/nav_bar.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
 module NavBar
-  class NavBar < ApplicationComponent; end
+  class NavBar < ApplicationComponent
+    def initialize(current_user:, **)
+      super
+
+      @current_user = current_user
+    end
+
+    private
+
+    attr_reader :current_user
+  end
 end
--- spec/components/nav_bar_spec.rb
+++ spec/components/nav_bar_spec.rb
@@ -5,6 +5,6 @@ require 'rails_helper'
 RSpec.describe NavBar::NavBar, type: :component do
   subject { render_inline(described_class.new(**params)) }
 
-  let(:params) { {} }
+  let(:params) { { current_user: create(:user) } }
   it { should have_css('.NavBar') }
 end
