d736bfe516dcf3b492147cbe0af0ee5606342a39
Till Prochaska <mail@tillprochaska.de>
2022-01-24 12:07:57 +0100

    Set human-readable server name in sentry config
    Fix #1192

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,4 +3,5 @@
 Sentry.init do |config|
   config.breadcrumbs_logger = [:active_support_logger]
   config.capture_exception_frame_locals = true
+  config.server_name = ENV['APPLICATION_HOSTNAME'] if ENV['APPLICATION_HOSTNAME'].present?
 end
