327454a23c4d757ffc53a7678746a568b9e3ff4f
Till Prochaska <mail@tillprochaska.de>
2022-03-30 14:20:32 +0200

    Move helper method to view component

--- app/components/onboarding_consent/onboarding_consent.rb
+++ app/components/onboarding_consent/onboarding_consent.rb
@@ -17,7 +17,7 @@ module OnboardingConsent
     end
 
     def display_additional_consent_checkbox?
-      Setting.onboarding_ask_for_additional_consent && Setting.onboarding_additional_consent_defined?
+      Setting.onboarding_ask_for_additional_consent? && Setting.onboarding_additional_consent_heading.strip.present?
     end
   end
 end
