7a4761d51b83d3345ae3193f91e246d9c5079b3d
Till Prochaska <mail@tillprochaska.de>
2022-05-19 13:25:10 +0200

    Fix rendering of onboarding logo fallback
    Previously, an image tag for the logo was rendered even if it didn’t exist. While fixing this, I also refactored the OnobardingHeader and OnboardingHero components and removed the `logo`/`hero` properties.

--- spec/components/onboarding_hero_spec.rb
+++ spec/components/onboarding_hero_spec.rb
@@ -3,8 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe OnboardingHero::OnboardingHero, type: :component do
-  subject { render_inline(described_class.new(**params)) }
-
-  let(:params) { { image: '/onboarding/photo.jpg' } }
-  it { should have_css('.OnboardingHero') }
+  subject { render_inline(described_class.new) }
+  it { should_not have_css('.OnboardingHero') }
 end
