b95ba817326f35207a66d3417253ef70b5c0c985
Till Prochaska <mail@tillprochaska.de>
2022-05-21 13:51:29 +0200

    Add component spec for `ImageInput`

--- spec/components/application_component_spec.rb
+++ spec/components/application_component_spec.rb
@@ -5,45 +5,40 @@ require 'rails_helper'
 RSpec.describe ApplicationComponent, type: :component do
   let(:params) { {} }
 
-  before(:all) do
-    button_class = Class.new(ApplicationComponent) do
-      def initialize(label: '', **)
+  let(:test_component) do
+    Class.new(ApplicationComponent) do
+      def initialize(some_property: '', **)
         super
-
-        @label = label
-      end
-
-      def call
-        button.tag(label, **attrs)
       end
     end
-
-    Object.const_set('Button', button_class)
   end
 
-  describe '#class_names' do
-    subject { Button.new(**params).send(:class_names) }
+  before(:each) { Object.const_set('TestComponent', test_component) }
+  after(:each) { Object.send(:remove_const, 'TestComponent') }
 
-    it { should eq(['Button']) }
+  describe '#class_names' do
+    subject { TestComponent.new(**params).send(:class_names) }
+
+    it { should eq(['TestComponent']) }
 
     describe 'given a list of styles' do
-      let(:params) { { styles: %i[large primary] } }
-      it { should eq(['Button', 'Button--large', 'Button--primary']) }
+      let(:params) { { styles: %i[style_a style_b] } }
+      it { should eq(['TestComponent', 'TestComponent--styleA', 'TestComponent--styleB']) }
     end
   end
 
   describe '#attrs' do
-    subject { Button.new(**params).send(:attrs).to_hash }
-    it { should eq({ class: 'Button' }) }
+    subject { TestComponent.new(**params).send(:attrs).to_hash }
+    it { should eq({ class: 'TestComponent' }) }
 
     context 'with attributes given explicitly' do
       let(:params) { { id: 'my-component' } }
-      it { should eq({ class: 'Button', id: 'my-component' }) }
+      it { should eq({ class: 'TestComponent', id: 'my-component' }) }
     end
 
     context 'with style, styles properties' do
-      let(:params) { { style: :large, styles: [:primary] } }
-      it { should eq({ class: 'Button Button--primary Button--large' }) }
+      let(:params) { { styles: [:style_a], style: :style_b } }
+      it { should eq({ class: 'TestComponent TestComponent--styleA TestComponent--styleB' }) }
     end
   end
 end
