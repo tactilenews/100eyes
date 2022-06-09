aedcfd3bc81ef012b0eda8d951ae657c1c3e8d3f
mattwr18 <mattwr18@gmail.com>
2022-06-09 18:06:46 +0200

    Add simple card to dashboard hero with empty state/list of notifications

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,10 @@
 class User < ApplicationRecord
   include Clearance::User
 
-  has_many :activity_notifications, as: :recipient
+  has_many :notifications,
+           as: :recipient,
+           class_name: 'ActivityNotification',
+           dependent: :destroy
 
   has_one_time_password
   validates :password, length: { in: 8..128 }, unless: :skip_password_validation?
--- spec/system/activity_notifications_spec.rb
+++ spec/system/activity_notifications_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe 'Activity Notifications' do
@@ -6,11 +8,20 @@ RSpec.describe 'Activity Notifications' do
     let(:password) { Faker::Internet.password(min_length: 8, max_length: 128) }
     let(:otp_enabled) { true }
     let(:user) { create(:user, email: email, password: password, otp_enabled: otp_enabled) }
-    let!(:activity_notification) { create(:activity_notification, recipient: user) }
-    
-    it "displays the activity notification on dashboard" do
+    let(:contributor) { create(:contributor) }
+    let!(:activity_notification) do
+      create(:activity_notification,
+             recipient: user,
+             type: OnboardingCompleted.name,
+             params: { contributor: contributor })
+    end
+
+    it 'displays the activity notification on dashboard' do
       visit dashboard_path(as: user)
-      expect(page).to have_text("Letzte Aktivität")
+      expect(page).to have_text('Letzte Aktivität')
+      expect(page).to have_text(
+        "#{contributor.name} hat sich via #{contributor.channels.first.to_s.capitalize} angemeldet."
+      )
     end
   end
 end
