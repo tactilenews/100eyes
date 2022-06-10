341c15919981ec5b4355778547fe7472d00473b3
mattwr18 <mattwr18@gmail.com>
2022-06-10 12:29:33 +0200

    Pass notifications from controller to view

--- app/components/hero/hero.rb
+++ app/components/hero/hero.rb
@@ -2,5 +2,12 @@
 
 module Hero
   class Hero < ApplicationComponent
+    def initialize(activity_notifications: [])
+      super
+
+      @activity_notifications = activity_notifications
+    end
+
+    attr_reader :activity_notifications
   end
 end
