e9476ee3bf2ec16e26f7494512ff7866c1856cb8
mattwr18 <mattwr18@gmail.com>
2022-06-15 15:12:16 +0200

    Revert changes to add ActivityFeed on Dashboard
    - We decided to put in a separate PR for the changes that will be
      visible to the user so that we can keep smaller PRs and facilitate the
    review process.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
 class DashboardController < ApplicationController
-  def index
-    @activity_notifications = current_user.notifications
-  end
+  def index; end
 end
