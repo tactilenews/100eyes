bff011df6e9f841d03673dcf95676c23bf7ad41d
mattwr18 <mattwr18@gmail.com>
2022-06-17 14:36:58 +0200

    Extract expectations to shared examples

--- spec/requests/onboarding/email_spec.rb
+++ spec/requests/onboarding/email_spec.rb
@@ -64,19 +64,8 @@ RSpec.describe 'Onboarding::Email', type: :request do
       expect(json_web_token).to exist
     end
 
-    context 'ActivityNotifications' do
-      let!(:users) { create_list(:user, 5) }
-
-      it 'creates an ActivityNotification of type OnboardingCompleted' do
-        expect { subject.call }.to change(ActivityNotification.where(type: 'OnboardingCompleted'), :count).by(User.count)
-      end
-
-      it 'creates an ActivityNotification for each user' do
-        subject.call
-        recipient_ids = ActivityNotification.where(type: 'OnboardingCompleted').pluck(:recipient_id)
-        user_ids = User.pluck(:id)
-        expect(recipient_ids).to eq(user_ids)
-      end
+    context 'creates an ActivityNotifications' do
+      include_examples 'activity_notifications'
     end
 
     context 'given invalid email address' do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -101,4 +101,6 @@ RSpec.configure do |config|
   config.after(:each) do
     Setting.clear_cache
   end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
 end
