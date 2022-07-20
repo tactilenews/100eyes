4bf5d9436133306dbf5050f2acfbecbcd7363066
mattwr18 <mattwr18@gmail.com>
2022-07-20 09:57:24 +0200

    Remove avatar

--- app/models/contributor.rb
+++ app/models/contributor.rb
@@ -14,6 +14,7 @@ class Contributor < ApplicationRecord
   has_many :received_messages, class_name: 'Message', inverse_of: :recipient, foreign_key: 'recipient_id', dependent: :destroy
   has_many :replied_to_requests, -> { reorder(created_at: :desc).distinct }, source: :request, through: :replies
   has_many :received_requests, -> { reorder(created_at: :desc).distinct }, source: :request, through: :received_messages
+  has_noticed_notifications model_name: 'ActivityNotification'
 
   has_one_attached :avatar
   has_one :json_web_token, dependent: :destroy
