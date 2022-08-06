0ef74cdf4182eb24924182664cafc4a663ceed1c
Theresa Hradilak <34538290+phoeinx@users.noreply.github.com>
2022-08-06 10:45:23 +0200

    Queue signal contact creation jobs in default queue.
    Co-authored-by: Till Prochaska <mail@tillprochaska.de>

--- app/jobs/signal_adapter/create_contact_job.rb
+++ app/jobs/signal_adapter/create_contact_job.rb
@@ -4,8 +4,6 @@ require 'net/http'
 
 module SignalAdapter
   class CreateContactJob < ApplicationJob
-    queue_as :create_signal_contact
-
     def perform(contributor)
       url = URI.parse("#{Setting.signal_cli_rest_api_endpoint}/v1/contacts/#{Setting.signal_server_phone_number}")
       header = {
