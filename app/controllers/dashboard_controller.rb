2a04ef4bc85c644237117d08bd29c32ccc2b16bd
mattwr18 <mattwr18@gmail.com>
2022-08-16 14:14:46 +0200

    Add store accessor to simplify code

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -6,9 +6,10 @@ class DashboardController < ApplicationController
     message_received = current_user.notifications.where(type: MessageReceived.name).newest_first
     @activity_notifications =
       if message_received.any?
-        grouped_by_request_id = message_received.group_by { |notification| notification.params[:request].id }
-        latest_message_received_per_request = grouped_by_request_id.map { |_key, value| value.first }
-        @count_per_request = grouped_by_request_id.map { |key, value| { key => value.count } }
+        grouped_by_request = message_received.group_by(&:request)
+        latest_message_received_per_request = grouped_by_request.map { |_key, value| value.first }
+        @count_per_request = grouped_by_request.map { |key, value| { key => value.count } }
+        # binding.pry
         (onboarding_completed + latest_message_received_per_request).sort_by(&:created_at).reverse!
       else
         onboarding_completed
--- app/models/activity_notification.rb
+++ app/models/activity_notification.rb
@@ -3,4 +3,5 @@
 class ActivityNotification < ApplicationRecord
   include Noticed::Model
   belongs_to :recipient, polymorphic: true
+  store_accessor :params, :request
 end
