a33310ae5f9cddce5181373ad8c53779376f76f1
mattwr18 <mattwr18@gmail.com>
2022-08-17 10:27:30 +0200

    Add scope for count_per_request to simplify logic

--- app/components/activity_notifications/activity_notifications.rb
+++ app/components/activity_notifications/activity_notifications.rb
@@ -2,20 +2,14 @@
 
 module ActivityNotifications
   class ActivityNotifications < ApplicationComponent
-    def initialize(activity_notifications: [], count_per_request: [])
+    delegate :current_user, to: :helpers
+
+    def initialize(activity_notifications: [])
       super
 
       @activity_notifications = activity_notifications
-      @count_per_request = count_per_request
     end
 
-    attr_reader :activity_notifications, :count_per_request
-
-    def count(request)
-      count_per_request.reduce(1) do |accumulator, count_hash|
-        key, value = count_hash.first
-        key == request ? value : accumulator
-      end
-    end
+    attr_reader :activity_notifications
   end
 end
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,12 +4,10 @@ class DashboardController < ApplicationController
   def index
     onboarding_completed = current_user.notifications.onboarding_completed.newest_first.limit(20)
     message_received = current_user.notifications.message_received.newest_first
-    @count_per_request = []
     @activity_notifications =
       if message_received.any?
         grouped_by_request = message_received.group_by(&:request)
         latest_message_received_per_request = grouped_by_request.map { |_key, value| value.first }
-        @count_per_request = grouped_by_request.map { |key, value| { key => value.count } }
         (onboarding_completed + latest_message_received_per_request).sort_by(&:created_at).reverse!
       else
         onboarding_completed
