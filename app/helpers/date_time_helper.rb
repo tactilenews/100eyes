9b3bba4609aa360a0e5e3634ba9d46087f2fd91f
mattwr18 <mattwr18@gmail.com>
2022-08-24 10:58:37 +0200

    Add time based greeting

--- app/helpers/date_time_helper.rb
+++ app/helpers/date_time_helper.rb
@@ -8,4 +8,23 @@ module DateTimeHelper
 
     I18n.l(date, format: :short)
   end
+
+  def time_based_heading
+    current_time = Time.current
+    midnight = current_time.beginning_of_day
+    noon = current_time.noon
+    evening = current_time.change(hour: 17)
+    night = current_time.change(hour: 20)
+
+    case current_time
+    when midnight..noon
+      '_morning'
+    when noon..evening
+      '_afternoon'
+    when evening..night
+      '_evening'
+    else
+      ''
+    end
+  end
 end
