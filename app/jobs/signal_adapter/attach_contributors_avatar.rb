aa3ae43a101af19b361eeb75bd9585d0ea220ec4
mattwr18 <mattwr18@gmail.com>
2022-09-08 16:17:22 +0200

    Lint and remove unintended code changes

--- app/jobs/signal_adapter/attach_contributors_avatar.rb
+++ app/jobs/signal_adapter/attach_contributors_avatar.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 module SignalAdapter
   class AttachContributorsAvatar < ApplicationJob
     queue_as :attach_signal_avatar
 
     def perform(contributor)
       avatar = "/app/signal-cli-config/avatars/profile-#{contributor.signal_phone_number}"
-      if File.file?(avatar)
-        contributor.avatar.attach(io: File.open(avatar), filename: "#{contributor.name.delete(' ').underscore}_profile_pic")
-      end
+      return unless File.file?(avatar)
+
+      contributor.avatar.attach(io: File.open(avatar), filename: "#{contributor.name.delete(' ').underscore}_profile_pic")
     end
   end
 end
