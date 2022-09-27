c4e39719ef12e930e0bebb3372e82dc9a36cecdc
mattwr18 <mattwr18@gmail.com>
2022-09-27 08:35:59 +0200

    Fix argument error

--- app/adapters/postmark_adapter/outbound.rb
+++ app/adapters/postmark_adapter/outbound.rb
@@ -65,8 +65,7 @@ module PostmarkAdapter
                 'message-id': "request/#{msg.request.id}/message/#{msg.id}@#{Setting.application_host}",
                 references: "request/#{msg.request.id}@#{Setting.application_host}"
               })
-      email_subject = "Re: #{localized_email_subject(msg.recipient.localization_tags&.first&.to_sym,
-                                                     I18n.t('adapter.postmark.new_message_email.subject'))}"
+      email_subject = "Re: #{localized_email_subject(msg.recipient.localization_tags&.first&.to_sym)}"
       message_stream = Setting.postmark_transactional_stream
       mail(to: msg.recipient.email, subject: email_subject, message_stream: message_stream)
     end
