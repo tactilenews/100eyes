3409243a50c846bda47a2f717ba4b6bd1ecc65e4
mattwr18 <mattwr18@gmail.com>
2022-09-29 12:03:40 +0200

    Revert unintended change

--- app/components/onboarding_consent/onboarding_consent.rb
+++ app/components/onboarding_consent/onboarding_consent.rb
@@ -12,11 +12,9 @@ module OnboardingConsent
 
     attr_reader :contributor
 
-    # rubocop:disable Rails/OutputSafety
     def data_processing_consent_help
-      I18n.t('contributor.form.data_processing_consent.help', link: Setting.onboarding_data_protection_link).html_safe
+      I18n.t('contributor.form.data_processing_consent.help', link: Setting.onboarding_data_protection_link)
     end
-    # rubocop:enable Rails/OutputSafety
 
     def display_additional_consent_checkbox?
       Setting.onboarding_ask_for_additional_consent? && Setting.onboarding_additional_consent_heading.strip.present?
