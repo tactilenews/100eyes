1cd14f4c3d129d04cd24a0f8403e5f94f6a7e946
mattwr18 <mattwr18@gmail.com>
2022-10-21 20:31:08 +0200

    Update count to include outbound chat messages

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
 class ChartsController < ApplicationController
-  content_security_policy only: :time_based_activity do |policy|
-    policy.style_src :self, :unsafe_inline
-  end
-  
   def time_based_activity
+    grouped_requests = Request.unscoped.group_by_hour_of_day(:created_at, format: '%H:%M').count
+    grouped_outbound_messages = Message.unscoped.where(sender_type: [User.name, nil], broadcasted: false).group_by_hour_of_day(:created_at, format: '%H:%M').count
+    joined_outbound = grouped_requests.merge(grouped_outbound_messages) { |_key, oldval, newval| oldval + newval }
     render json: [{ 
-      name: 'Eingegangene', data: Message.unscoped.replies.group_by_day_of_week(:created_at, format: '%A').count },
-      { name: 'Fragen', data: Request.unscoped.group_by_day_of_week(:created_at, format: '%A').count }]
+      name: 'Eingegangene', data: Message.unscoped.replies.group_by_hour_of_day(:created_at, format: '%H:%M').count },
+      { name: 'Fragen', data: joined_outbound }]
   end
 end
