7a0ca8d7494967d5eaae4ca5bcc76c5156dbbe0c
mattwr18 <mattwr18@gmail.com>
2022-10-21 20:30:15 +0200

    Update security policy that re-generates a code on page change
    - we need to persist the same nonce per request to avoid not rendering
      the chart when navigating to other pages

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -22,7 +22,7 @@ Rails.application.config.content_security_policy do |policy|
 end
 
 # If you are using UJS then enable automatic nonce generation
-Rails.application.config.content_security_policy_nonce_generator = ->(_request) { SecureRandom.base64(16) }
+Rails.application.config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
 
 # Set the nonce only to specific directives
 Rails.application.config.content_security_policy_nonce_directives = %w[script-src]
