873b9ec4c72f13ce93093a92d86a013ffb243d1c
mattwr18 <mattwr18@gmail.com>
2022-11-21 14:54:44 +0100

    Lint

--- spec/adapters/telegram_adapter/outbound/photo_spec.rb
+++ spec/adapters/telegram_adapter/outbound/photo_spec.rb
@@ -9,27 +9,25 @@ RSpec.describe TelegramAdapter::Outbound::Photo do
   let(:photos) { create_list(:file, 2, message: message) }
   let(:media) { photos.map { |photo| ActiveStorage::Blob.service.path_for(photo.attachment.blob.key) } }
 
-
   describe '#perform' do
     before do
       allow(File).to receive(:open).and_call_original
       allow(File).to receive(:open)
-            .with(media.first)
-            .and_return(ActiveStorage::Blob.service.path_for(photos.first.attachment.blob.key))
+        .with(media.first)
+        .and_return(ActiveStorage::Blob.service.path_for(photos.first.attachment.blob.key))
       allow(File).to receive(:open)
-            .with(media.second)
-            .and_return(ActiveStorage::Blob.service.path_for(photos.second.attachment.blob.key))
+        .with(media.second)
+        .and_return(ActiveStorage::Blob.service.path_for(photos.second.attachment.blob.key))
     end
 
     subject { adapter.perform(telegram_id: message.recipient.telegram_id, media: media, message: message) }
-    let(:expected_message) do 
+    let(:expected_message) do
       { chat_id: 4,
-      media: [
-        { type: 'photo', media: File.open(media.first), caption: message.text },
-        { type: 'photo', media: File.open(media.second), caption: '' },
+        media: [
+          { type: 'photo', media: File.open(media.first), caption: message.text },
+          { type: 'photo', media: File.open(media.second), caption: '' }
         ],
-        parse_mode: :HTML
-      }
+        parse_mode: :HTML }
     end
 
     it 'sends the message with TelegramBot' do
