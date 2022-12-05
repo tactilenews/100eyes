1aa2eb7c798874ebf3b22cfe362ba456c4412382
mattwr18 <mattwr18@gmail.com>
2022-12-05 12:02:37 +0100

    Add users section

--- spec/system/profile/index_spec.rb
+++ spec/system/profile/index_spec.rb
@@ -12,16 +12,24 @@ RSpec.describe 'Profile' do
   end
   let(:business_plan) { create(:business_plan) }
   let(:contact_person) { create(:user, first_name: 'Isaac', last_name: 'Bonga') }
-  let(:organization) { create(:organization, business_plan: business_plan, contact_person: contact_person) }
+  let(:organization) { create(:organization, business_plan: business_plan, contact_person: contact_person, users_count: 2) }
 
   it 'allows viewing/updating business plan' do
     visit profile_path(as: user)
 
+    # header
     expect(page).to have_content("Dein 100eyes Plan: \"#{business_plan.name}\"")
     expect(page).to have_content("Auftraggeber:in #{organization.contact_person.name}, #{organization.contact_person.email}")
     expect(page).to have_content("Preis: #{number_to_currency(business_plan.price_per_month)}/Monat")
     expect(page).to have_content("Mindeslaufzeit: bis #{business_plan.valid_until.strftime('%m/%Y')}")
     expect(page).to have_content('Dialogkanäle: Signal, Threema, Telegram, E-mail')
     expect(page).to have_content('Sicherheit: Community abgesichert über Zwei-Faktor-Authentifizierung, Cloudflare')
+
+    # user section
+    expect(page).to have_content('Deine Redakteur:Innen')
+    expect(page).to have_content("#{organization.users.count} von #{organization.business_plan.number_of_users} Seats genutzt")
+    organization.users.each do |user|
+      expect(page).to have_content(user.name)
+    end
   end
 end
