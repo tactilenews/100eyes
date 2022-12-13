378bc47cc9f32cccef0c89dce0951114a7059028
mattwr18 <mattwr18@gmail.com>
2022-12-13 11:51:06 +0100

    Unify button style, allow copy button icon to be configurable

--- app/components/copy_button/copy_button.rb
+++ app/components/copy_button/copy_button.rb
@@ -2,20 +2,21 @@
 
 module CopyButton
   class CopyButton < ApplicationComponent
-    def initialize(copy: nil, url: nil, key: nil, label: nil, loading: nil, success: nil, **)
+    def initialize(copy: nil, url: nil, key: nil, label: nil, loading: nil, success: nil, icon_name: :ungroup, **)
       super
 
       @copy = copy
       @url = url
       @key = key
       @label = label
+      @icon_name = icon_name
       @loading = loading || I18n.t('components.copy_button.loading')
       @success = success || I18n.t('components.copy_button.success')
     end
 
     private
 
-    attr_reader :copy, :url, :key, :label, :loading, :success
+    attr_reader :copy, :url, :key, :label, :loading, :success, :icon_name
 
     def attrs
       super.defaults(
