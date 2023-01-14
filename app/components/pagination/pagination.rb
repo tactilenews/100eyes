c11ab5c124416e3e76877da69b1dc68ed6c4c136
mattwr18 <mattwr18@gmail.com>
2023-01-14 11:28:38 +0100

    Make path configurable

--- app/components/pagination/pagination.rb
+++ app/components/pagination/pagination.rb
@@ -2,14 +2,19 @@
 
 module Pagination
   class Pagination < ApplicationComponent
-    def initialize(current_page:, remote:, pages:)
+    def initialize(current_page:, remote:, pages:, path:)
       super
 
       @current_page = current_page
       @remote = remote
       @pages = pages
+      @path = path
     end
 
-    attr_reader :current_page, :remote, :pages
+    attr_reader :current_page, :remote, :pages, :path
+
+    def path_to(page:)
+      "#{path}/page/#{page}"
+    end
   end
 end
