31a86c4fe8cb3bb7e0c0ef3c63ceb281b71ef344
mattwr18 <mattwr18@gmail.com>
2023-01-17 14:03:57 +0100

    Add request specs

---
+++ spec/requests/whats_app/webhook_spec.rb
@@ -0,0 +1,103 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe WhatsApp::WebhookController do
+  let(:auth_token) { 'valid_auth_token' }
+  let(:mock_twilio_security_request_validator) { instance_double(Twilio::Security::RequestValidator) }
+  let(:whats_app_phone_number) { '+491511234567' }
+  let(:params) do
+    {
+      'AccountSid' => 'someAccount',
+      'ApiVersion' => '2010-04-01',
+      'Body' => 'Hello',
+      'From' => "whatsapp:#{whats_app_phone_number}",
+      'MessageSid' => 'someId',
+      'NumMedia' => '0',
+      'NumSegments' => '1',
+      'ProfileName' => 'Matthew Rider',
+      'ReferralNumMedia' => '0',
+      'SmsMessageSid' => 'someId',
+      'SmsSid' => 'someId',
+      'SmsStatus' => 'received',
+      'To' => "whatsapp:#{Setting.whats_app_server_phone_number}",
+      'WaId' => '491511234567'
+    }
+  end
+
+  subject { -> { post whats_app_webhook_path, params: params } }
+
+  describe '#message' do
+    before do
+      allow(Sentry).to receive(:capture_exception)
+      allow(Setting).to receive(:whats_app_server_phone_number).and_return('4915133311445')
+      allow(Twilio::Security::RequestValidator).to receive(:new).and_return(mock_twilio_security_request_validator)
+    end
+
+    describe 'fails Rack::TwilioWebhookAuthentication' do
+      before do
+        allow(mock_twilio_security_request_validator).to receive(:validate).and_return(false)
+      end
+
+      it 'returns forbidden' do
+        subject.call
+        expect(response).to have_http_status(:forbidden)
+      end
+
+      it 'returns message why it failed' do
+        subject.call
+        expect(response.body).to eq('Twilio Request Validation Failed.')
+      end
+    end
+
+    describe 'unknown contributor' do
+      before do
+        allow(mock_twilio_security_request_validator).to receive(:validate).and_return(true)
+      end
+
+      it 'does not create a message' do
+        expect { subject.call }.not_to change(Message, :count)
+      end
+
+      it 'raises an error' do
+        expect(Sentry).to receive(:capture_exception).with(
+          WhatsAppAdapter::UnknownContributorError.new(whats_app_phone_number: '49151123456')
+        )
+
+        subject.call
+      end
+    end
+
+    describe 'given a contributor' do
+      let!(:contributor) { create(:contributor, whats_app_phone_number: whats_app_phone_number) }
+
+      before do
+        allow(mock_twilio_security_request_validator).to receive(:validate).and_return(true)
+        create(:message, request: create(:request), recipient: contributor)
+      end
+
+      context 'no message template set' do
+        it 'creates a messsage' do
+          expect { subject.call }.to change(Message, :count).from(1).to(2)
+        end
+      end
+
+      context 'responding to template' do
+        before { contributor.update(whats_app_template_message_sent_at: Time.current) }
+        let(:expected_job_args) { { recipient: contributor, text: contributor.received_messages.first.text } }
+
+        it 'sets `latest_message_received_at` to open 24-hour window of free-form messages' do
+          expect { subject.call }.to change { contributor.reload.latest_message_received_at }.from(nil).to(kind_of(Time))
+        end
+
+        it 'set `whats_app_template_message_sent_at` to nil' do
+          expect { subject.call }.to change { contributor.reload.whats_app_template_message_sent_at }.from(kind_of(Time)).to(nil)
+        end
+
+        it 'enqueues a job to send the latest received message' do
+          expect { subject.call }.to have_enqueued_job(WhatsAppAdapter::Outbound::Text).on_queue('default').with(expected_job_args)
+        end
+      end
+    end
+  end
+end
