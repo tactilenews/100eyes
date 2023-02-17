a1350aca22a5f928cfc32d8f35cd6a4d3c997208
mattwr18 <mattwr18@gmail.com>
2023-02-17 11:15:41 +0100

    Simplify outbound message logic

--- app/adapters/whats_app_adapter/outbound.rb
+++ app/adapters/whats_app_adapter/outbound.rb
@@ -6,19 +6,10 @@ module WhatsAppAdapter
       recipient = message&.recipient
       return unless contributor_can_receive_messages?(recipient)
 
-      if send_message_template?(recipient)
-        recipient.update(whats_app_template_message_sent_at: Time.current)
-
-        text = I18n.t("adapter.whats_app.request_template.new_request_#{time_of_day}_#{rand(1..3)}", first_name: recipient.first_name,
-                                                                                                     request_title: message.request.title)
-        send_text(recipient, text) and return
-      end
-      files = message.files
-
-      if files.present?
-        send_files(files, recipient, message.text)
+      if freeform_message_permitted?(recipient)
+        send_message(recipient, message)
       else
-        send_text(recipient, message.text)
+        send_message_template(recipient, message)
       end
     end
 
@@ -42,16 +33,6 @@ module WhatsAppAdapter
       recipient&.whats_app_phone_number.present?
     end
 
-    def self.send_files(files, recipient, text)
-      files.each_with_index do |file, index|
-        WhatsAppAdapter::Outbound::File.perform_later(recipient: recipient, text: index.zero? ? text : '', file: file)
-      end
-    end
-
-    def self.send_text(recipient, text)
-      WhatsAppAdapter::Outbound::Text.perform_later(recipient: recipient, text: text)
-    end
-
     def self.time_of_day
       current_time = Time.current
       morning = current_time.change(hour: 6)
@@ -71,14 +52,31 @@ module WhatsAppAdapter
       end
     end
 
-    def self.send_message_template?(recipient)
-      responded_to_template_message_at = recipient.whats_app_message_template_responded_at
-      latest_message_received_at = recipient.replies.first&.created_at
+    def self.freeform_message_permitted?(recipient)
+      responding_to_template_message = recipient.whats_app_message_template_responded_at.present?
+      latest_message_received_more_24_hours_ago = recipient.replies.first&.created_at.present? &&
+                                                  recipient.replies.first.created_at > 24.hours.ago
+      responding_to_template_message || latest_message_received_more_24_hours_ago
+    end
 
-      return false if latest_message_received_at.present? && latest_message_received_at > 24.hours.ago
-      return false if responded_to_template_message_at.present? && responded_to_template_message_at > 24.hours.ago
+    def self.send_message_template(recipient, message)
+      recipient.update(whats_app_template_message_sent_at: Time.current)
 
-      true
+      text = I18n.t("adapter.whats_app.request_template.new_request_#{time_of_day}_#{rand(1..3)}", first_name: recipient.first_name,
+                                                                                                   request_title: message.request.title)
+      WhatsAppAdapter::Outbound::Text.perform_later(recipient: recipient, text: text)
+    end
+
+    def self.send_message(recipient, message)
+      files = message.files
+
+      if files.blank?
+        WhatsAppAdapter::Outbound::Text.perform_later(recipient: recipient, text: message.text)
+      else
+        files.each_with_index do |file, index|
+          WhatsAppAdapter::Outbound::File.perform_later(recipient: recipient, text: index.zero? ? message.text : '', file: file)
+        end
+      end
     end
   end
 end
