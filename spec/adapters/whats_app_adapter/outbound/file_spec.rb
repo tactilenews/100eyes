a3e5e39e6a502660047708fbe0d3e23ea6325514
mattwr18 <mattwr18@gmail.com>
2023-03-15 09:29:49 +0100

    Fix specs after favoring api key integration

--- spec/adapters/whats_app_adapter/outbound/file_spec.rb
+++ spec/adapters/whats_app_adapter/outbound/file_spec.rb
@@ -6,7 +6,8 @@ RSpec.describe WhatsAppAdapter::Outbound::File do
   let(:adapter) { described_class.new }
   let(:whats_app_phone_number) { '+491511234567' }
   let(:valid_account_sid) { 'VALID_ACCOUNT_SID' }
-  let(:valid_auth_token) { 'VALID_AUTH_TOKEN' }
+  let(:valid_api_key_sid) { 'VALID_API_KEY_SID' }
+  let(:valid_api_key_secret) { 'VALID_API_KEY_SECRET' }
   let(:mock_twilio_rest_client) { instance_double(Twilio::REST::Client) }
   let(:messages_double) { double(Twilio::REST::Api::V2010::AccountContext::MessageInstance) }
   let(:contributor) do
@@ -25,8 +26,10 @@ RSpec.describe WhatsAppAdapter::Outbound::File do
 
     before do
       allow(Setting).to receive(:twilio_account_sid).and_return(valid_account_sid)
-      allow(Setting).to receive(:twilio_auth_token).and_return(valid_auth_token)
-      allow(Twilio::REST::Client).to receive(:new).with(valid_account_sid, valid_auth_token).and_return(mock_twilio_rest_client)
+      allow(Setting).to receive(:twilio_api_key_sid).and_return(valid_api_key_sid)
+      allow(Setting).to receive(:twilio_api_key_secret).and_return(valid_api_key_secret)
+      allow(Twilio::REST::Client).to receive(:new).with(valid_api_key_sid, valid_api_key_secret,
+                                                        valid_account_sid).and_return(mock_twilio_rest_client)
       allow(mock_twilio_rest_client).to receive(:messages).and_return(messages_double)
       allow(messages_double).to receive(:create)
     end
