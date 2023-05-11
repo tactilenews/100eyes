2cd98ef3355e0b5529470bda8f03c2dd504ea361
mattwr18 <mattwr18@gmail.com>
2023-05-11 07:44:09 +0200

    Update create api key flow

--- app/jobs/whats_app_adapter/create_api_key.rb
+++ app/jobs/whats_app_adapter/create_api_key.rb
@@ -3,16 +3,16 @@
 require 'net/http'
 
 module WhatsAppAdapter
-  class CreateApiKey
+  class CreateApiKey < ApplicationJob 
     def perform(channel_id:)
-      url = URI.parse(" https://hub.360dialog.io/api/v2/partners/#{ENV.fetch('360_DIALOG_PARTNER_ID', '')}/channels/#{channel_id}/api_keys")
+      url = URI.parse("https://hub.360dialog.io/api/v2/partners/#{ENV.fetch('360_DIALOG_PARTNER_ID', '')}/channels/#{channel_id}/api_keys")
       headers = {
         Accept: 'application/json',
         'Content-Type': 'application/json',
         Authorization: "Bearer #{ENV.fetch('360_DIALOG_ACCESS_TOKEN', '')}"
       }
       request = Net::HTTP::Post.new(url.to_s, headers)
-      response = Net::HTTP.start(url.host, url.port) do |http|
+      response = Net::HTTP.start(url.host, url.port, use_ssl: true) do |http|
         http.request(request)
       end
       api_key = response.body['api_key']
