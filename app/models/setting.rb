cd774668ffaad89495f445c0c9a6d3751c1d4b4b
mattwr18 <mattwr18@gmail.com>
2023-08-08 20:35:54 +0200

    Fix naming issues with numerical 360

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -73,16 +73,17 @@ class Setting < RailsSettings::Base
   field :whats_app_server_phone_number, readonly: true, default: ENV.fetch('WHATS_APP_SERVER_PHONE_NUMBER', nil)
 
   field :three_sixty_dialog_partner_token
-  field :three_sixty_dialog_partner_id, readonly: true, default: ENV.fetch('360_DIALOG_PARTNER_ID', nil)
-  field :three_sixty_dialog_partner_username, readonly: true, default: ENV.fetch('360_DIALOG_PARTNER_USERNAME', nil)
-  field :three_sixty_dialog_partner_password, readonly: true, default: ENV.fetch('360_DIALOG_PARTNER_PASSWORD', nil)
-  field :three_sixty_dialog_partner_rest_api_endpoint, readonly: true, default: ENV.fetch('360_DIALOG_PARTNER_REST_API_ENDPOINT', 'https://stoplight.io/mocks/360dialog/360dialog-partner-api/24588693')
+  field :three_sixty_dialog_partner_id, readonly: true, default: ENV.fetch('THREE_SIXTY_DIALOG_PARTNER_ID', nil)
+  field :three_sixty_dialog_partner_username, readonly: true, default: ENV.fetch('THREE_SIXTY_DIALOG_PARTNER_USERNAME', nil)
+  field :three_sixty_dialog_partner_password, readonly: true, default: ENV.fetch('THREE_SIXTY_DIALOG_PARTNER_PASSWORD', nil)
+  field :three_sixty_dialog_partner_rest_api_endpoint, readonly: true, default: ENV.fetch('THREE_SIXTY_DIALOG_PARTNER_REST_API_ENDPOINT', 'https://stoplight.io/mocks/360dialog/360dialog-partner-api/24588693')
 
   field :three_sixty_dialog_client_api_key
   field :three_sixty_dialog_client_id
   field :three_sixty_dialog_client_waba_account_id
 
-  field :three_sixty_dialog_whats_app_rest_api_endpoint, readonly: true, default: ENV.fetch('360_DIALOG_WHATS_APP_REST_API_ENDPOINT', 'https://waba-sandbox.360dialog.io')
+  field :three_sixty_dialog_whats_app_rest_api_endpoint, readonly: true,
+                                                         default: ENV.fetch('THREE_SIXTY_DIALOG_WHATS_APP_REST_API_ENDPOINT', 'https://waba-sandbox.360dialog.io')
   field :three_sixty_dialog_whats_app_template_namespace
 
   field :inbound_email_password, readonly: true, default: ENV.fetch('RAILS_INBOUND_EMAIL_PASSWORD', nil)
