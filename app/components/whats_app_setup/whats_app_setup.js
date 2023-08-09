42f5ecb0d4d5b3e5eba530e379e1b5365452999f
mattwr18 <mattwr18@gmail.com>
2023-08-09 20:27:44 +0200

    Move connect button to profile, use JS/modal

---
+++ app/components/whats_app_setup/whats_app_setup.js
@@ -0,0 +1,21 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+  static values = {
+    permissionsUrl: String,
+  };
+
+  connect() {
+    console.log('connected');
+  }
+
+  openModal() {
+    const windowFeatures =
+      'toolbar=no, menubar=no, width=600, height=900, top=100, left=100';
+    open(
+      this.permissionsUrlValue,
+      'integratedOnboardingWindow',
+      windowFeatures
+    );
+  }
+}
