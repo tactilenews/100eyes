176fbf31fe22224107bfba01ebb35423c34f62d5
mattwr18 <mattwr18@gmail.com>
2023-08-23 19:04:17 +0200

    Return class from helper method instead of substring
    - follow @roschaefer PR suggestion and simplify the code

--- app/adapters/whats_app_adapter/outbound.rb
+++ app/adapters/whats_app_adapter/outbound.rb
@@ -4,25 +4,25 @@ module WhatsAppAdapter
   class Outbound
     class << self
       def send!(message)
-        "WhatsAppAdapter::#{business_solution_provider}Outbound".constantize.send!(message)
+        business_solution_provider.constantize.send!(message)
       end
 
       def send_welcome_message!(contributor)
-        "WhatsAppAdapter::#{business_solution_provider}Outbound".constantize.send_welcome_message!(contributor)
+        business_solution_provider.constantize.send_welcome_message!(contributor)
       end
 
       def send_more_info_message!(contributor)
-        "WhatsAppAdapter::#{business_solution_provider}Outbound".constantize.send_more_info_message!(contributor)
+        business_solution_provider.constantize.send_more_info_message!(contributor)
       end
 
       def send_unsubsribed_successfully_message!(contributor)
-        "WhatsAppAdapter::#{business_solution_provider}Outbound".constantize.send_unsubsribed_successfully_message!(contributor)
+        business_solution_provider.constantize.send_unsubsribed_successfully_message!(contributor)
       end
 
       private
 
       def business_solution_provider
-        Setting.three_sixty_dialog_client_api_key.present? ? 'ThreeSixtyDialog' : 'Twilio'
+        Setting.three_sixty_dialog_client_api_key.present? ? WhatsAppAdapter::ThreeSixtyDialogOutbound : WhatsAppAdapter::TwilioOutbound
       end
     end
   end
