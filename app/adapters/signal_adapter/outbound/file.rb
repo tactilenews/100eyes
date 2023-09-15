7cea0c332fb35f0ab7b07f6bed8657f08f1b9d82
Robert Schäfer <git@roschaefer.de>
2023-09-15 16:07:39 +0200

    refactor: DRY `handle_response`
    I use the same pattern from this PR here: https://github.com/tactilenews/100eyes/pull/1686
    
    Looks like it's always better to pass `URI` instead of `string`: https://ruby-doc.org/stdlib-2.4.1/libdoc/net/http/rdoc/Net/HTTPGenericRequest.html
    Because `@uri` and `@host` get initialized.

--- app/adapters/signal_adapter/outbound/file.rb
+++ app/adapters/signal_adapter/outbound/file.rb
@@ -9,16 +9,17 @@ module SignalAdapter
 
       def perform(message:)
         @message = message
-        url = URI.parse("#{Setting.signal_cli_rest_api_endpoint}/v2/send")
-        request = Net::HTTP::Post.new(url.to_s, {
+        uri = URI.parse("#{Setting.signal_cli_rest_api_endpoint}/v2/send")
+        request = Net::HTTP::Post.new(uri, {
                                         Accept: 'application/json',
                                         'Content-Type': 'application/json'
                                       })
         request.body = data.to_json
-        response = Net::HTTP.start(url.host, url.port) do |http|
-          http.request(request)
+        SignalAdapter::Api.perform_request(request) do
+          # TODO: Do something on success. For example, mark the message as delivered?
+          # Or should we use deliver receipts as the source of truth.
+          Rails.logger.debug 'Great!'
         end
-        handle_response(response)
       end
 
       def data
@@ -31,25 +32,6 @@ module SignalAdapter
           message: message.text,
           base64_attachments: base64_files
         }
-      end
-
-      def handle_response(response)
-        case response.code.to_i
-        when 200
-          # TODO: Do something on success. For example, mark the message as delivered?
-          # Or should we use deliver receipts as the source of truth.
-          Rails.logger.debug 'Great!'
-        when 400..599
-          error_message = JSON.parse(response.body)['error']
-          exception = SignalAdapter::BadRequestError.new(error_code: response.code, message: error_message)
-          context = {
-            code: response.code,
-            message: response.message,
-            headers: response.to_hash,
-            body: error_message
-          }
-          ErrorNotifier.report(exception, context: context)
-        end
       end
     end
   end
--- app/adapters/signal_adapter/outbound/text.rb
+++ app/adapters/signal_adapter/outbound/text.rb
@@ -10,16 +10,17 @@ module SignalAdapter
       def perform(recipient:, text:)
         @recipient = recipient
         @text = text
-        url = URI.parse("#{Setting.signal_cli_rest_api_endpoint}/v2/send")
-        request = Net::HTTP::Post.new(url.to_s, {
+        uri = URI.parse("#{Setting.signal_cli_rest_api_endpoint}/v2/send")
+        request = Net::HTTP::Post.new(uri, {
                                         Accept: 'application/json',
                                         'Content-Type': 'application/json'
                                       })
         request.body = data.to_json
-        response = Net::HTTP.start(url.host, url.port) do |http|
-          http.request(request)
+        SignalAdapter::Api.perform_request(request) do
+          # TODO: Do something on success. For example, mark the message as delivered?
+          # Or should we use deliver receipts as the source of truth.
+          Rails.logger.debug 'Great!'
         end
-        handle_response(response)
       end
 
       def data
@@ -28,25 +29,6 @@ module SignalAdapter
           recipients: [recipient.signal_phone_number],
           message: text
         }
-      end
-
-      def handle_response(response)
-        case response.code.to_i
-        when 200
-          # TODO: Do something on success. For example, mark the message as delivered?
-          # Or should we use deliver receipts as the source of truth.
-          Rails.logger.debug 'Great!'
-        when 400..599
-          error_message = JSON.parse(response.body)['error']
-          exception = SignalAdapter::BadRequestError.new(error_code: response.code, message: error_message)
-          context = {
-            code: response.code,
-            message: response.message,
-            headers: response.to_hash,
-            body: error_message
-          }
-          ErrorNotifier.report(exception, context: context)
-        end
       end
     end
   end
