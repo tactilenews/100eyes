ec61837842bf39275142150c98548fe1cc406801
mattwr18 <mattwr18@gmail.com>
2023-12-15 15:07:56 +0100

    Save Twilio MessageSid as external_id when message exists

--- app/adapters/whats_app_adapter/outbound/text.rb
+++ app/adapters/whats_app_adapter/outbound/text.rb
@@ -9,15 +9,18 @@ module WhatsAppAdapter
         @twilio_instance = Twilio::REST::Client.new(Setting.twilio_api_key_sid, Setting.twilio_api_key_secret, Setting.twilio_account_sid)
       end
 
-      def perform(contributor_id:, text:)
+      def perform(contributor_id:, text:, message: nil)
         contributor = Contributor.find(contributor_id)
         return unless contributor
 
-        self.class.twilio_instance.messages.create(
+        response = self.class.twilio_instance.messages.create(
           from: "whatsapp:#{Setting.whats_app_server_phone_number}",
           body: text,
           to: "whatsapp:#{contributor.whats_app_phone_number}"
         )
+        return unless message
+
+        message.update(external_id: response.sid)
       end
     end
   end
