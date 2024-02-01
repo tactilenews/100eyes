0edade3a5f14f2d4ad8f200d6b9c1517761d25d7
mattwr18 <mattwr18@gmail.com>
2024-02-01 11:02:08 +0100

    Reduce notification locations for Bullet

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,11 +5,8 @@ require_relative '../../app/models/setting'
 Rails.application.configure do
   config.after_initialize do
     Bullet.enable        = true
-    Bullet.alert         = true
     Bullet.bullet_logger = true
-    Bullet.console       = true
     Bullet.rails_logger  = true
-    Bullet.add_footer    = true
   end
 
   # Settings specified here will take precedence over those in config/application.rb.
