72ecef7cf6495aea5de1efb8dafe83e9484c0c4a
mattwr18 <mattwr18@gmail.com>
2024-02-28 11:38:15 +0100

    Add MessageGroupsSkeleton component

--- app/components/message_groups/message_groups.rb
+++ app/components/message_groups/message_groups.rb
@@ -2,14 +2,13 @@
 
 module MessageGroups
   class MessageGroups < ApplicationComponent
-    def initialize(request:, message_groups: [], path: nil)
+    def initialize(request:, message_groups: [])
       super
 
       @request = request
       @message_groups = message_groups
-      @path = path
     end
 
-    attr_reader :request, :message_groups, :path
+    attr_reader :request, :message_groups
   end
 end
