2e5a6cefc11eb2472549e57b9b380e5408c7f148
mattwr18 <mattwr18@gmail.com>
2024-03-14 12:48:18 +0100

    Add pagination to contributors index

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -87,6 +87,6 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   config.include ActionView::Helpers::NumberHelper, type: :system
   config.include ActionView::Helpers::NumberHelper, type: :requests
-
+  config.include Rails.application.routes.url_helpers, type: :component
   config.include Helpers
 end
