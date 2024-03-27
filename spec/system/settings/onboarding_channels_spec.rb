025c5d2532565cdfc9001b215d05e547fd7bc57e
mattwr18 <mattwr18@gmail.com>
2024-03-27 20:03:41 +0100

    Fix spec that expects all channels to be configured by default

--- spec/system/settings/onboarding_channels_spec.rb
+++ spec/system/settings/onboarding_channels_spec.rb
@@ -10,15 +10,56 @@ RSpec.describe 'Configuring Onboarding Channels' do
     JsonWebToken.encode(jwt_payload)
   end
 
+  before do
+    allow(Setting).to receive(:threemarb_api_identity).and_return(nil)
+    allow(Setting).to receive(:telegram_bot_api_key).and_return(nil)
+    allow(Setting).to receive(:signal_server_phone_number).and_return(nil)
+    allow(Setting).to receive(:whats_app_configured?).and_return(false)
+    # defaults to true as Postmark handles user management
+    allow(Setting).to receive(:postmark_api_token).and_return('valid_api_token')
+  end
+
   it 'allows activating and deactivating onboarding channels' do
     visit settings_path(as: admin)
 
-    # All activated by default
+    # With no messengers configured
     within('.OnboardingChannelsCheckboxes') do
       Setting.channels.each_key do |key|
-        expect(page).to have_field(key.to_s.camelize, id: "setting[channels][#{key}]", checked: true)
+        checked_status = !key.eql?(:email)
+        expect(page).to have_field(key.to_s.camelize, id: "setting[channels][#{key}]", checked: checked_status)
       end
+    end
 
+    allow(Setting).to receive(:threemarb_api_identity).and_return('valid_api_identity')
+    visit settings_path(as: admin)
+
+    within('.OnboardingChannelsCheckboxes') do
+      expect(page).to have_field('Threema', id: 'setting[channels][threema]', checked: true)
+    end
+
+    allow(Setting).to receive(:telegram_bot_api_key).and_return(nil)
+    visit settings_path(as: admin)
+
+    within('.OnboardingChannelsCheckboxes') do
+      expect(page).to have_field('Telegram', id: 'setting[channels][telegram]', checked: true)
+    end
+
+    allow(Setting).to receive(:signal_server_phone_number).and_return('+491234567')
+    visit settings_path(as: admin)
+
+    within('.OnboardingChannelsCheckboxes') do
+      expect(page).to have_field('Signal', id: 'setting[channels][signal]', checked: true)
+    end
+
+    allow(Setting).to receive(:whats_app_configured?).and_return(true)
+    visit settings_path(as: admin)
+
+    within('.OnboardingChannelsCheckboxes') do
+      expect(page).to have_field('WhatsApp', id: 'setting[channels][whats_app]', checked: true)
+    end
+
+    # Uncheck email
+    within('.OnboardingChannelsCheckboxes') do
       uncheck 'Email'
     end
 
