03cb39f6d6ccfd58499d79fd6b884750b29e82f2
mattwr18 <mattwr18@gmail.com>
2024-04-19 08:01:27 +0200

    Favor component spec over outdated view spec
    we favor view_components in part because of their ease in testability

---
+++ spec/components/contributors_index_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ContributorsIndex::ContributorsIndex, type: :component do
+  subject { render_inline(described_class.new(**params)) }
+
+  let(:params) do
+    {
+      contributors: Kaminari.paginate_array(create_list(:contributor, 3)).page(1),
+      state: :active,
+      active_count: 2,
+      inactive_count: 1,
+      unsubscribed_count: 0,
+      filter_count: 0,
+      tag_list: []
+    }
+  end
+
+  it { is_expected.to have_css('.ContributorsIndex') }
+end
