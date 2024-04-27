1eb1e30a7b6c5253c031d0b7cb74a510e50a109e
mattwr18 <mattwr18@gmail.com>
2024-04-27 09:12:05 +0200

    Add specs for saving external_id for Threema send

--- spec/adapters/threema_adapter/outbound/file_spec.rb
+++ spec/adapters/threema_adapter/outbound/file_spec.rb
@@ -21,11 +21,16 @@ RSpec.describe ThreemaAdapter::Outbound::File do
       caption: message.text
     }
   end
+  let(:message_id) { SecureRandom.alphanumeric(16) }
+  let(:threema_lookup_double) { instance_double(Threema::Lookup) }
 
   describe '#perform' do
     before do
       message.reload
-      allow(Threema).to receive(:new).and_return(threema_double)
+      allow(ThreemaAdapter::Outbound::File).to receive(:threema_instance).and_return(threema_double)
+      allow(Threema::Lookup).to receive(:new).and_call_original
+      allow(Threema::Lookup).to receive(:new).with({ threema: threema_double }).and_return(threema_lookup_double)
+      allow(threema_lookup_double).to receive(:key).and_return('PUBLIC_KEY_HEX_ENCODED')
     end
     subject do
       lambda {
@@ -41,6 +46,27 @@ RSpec.describe ThreemaAdapter::Outbound::File do
       expect(threema_double).to receive(:send).with(expected_params)
 
       subject.call
+    end
+
+    context 'when a message is passed in' do
+      subject do
+        lambda {
+          adapter.perform(contributor_id: message.recipient.id,
+                          file_path: file_path,
+                          file_name: message.files.first.attachment.blob.filename.to_s,
+                          caption: message.text,
+                          render_type: :media,
+                          message: message)
+        }
+      end
+
+      before do
+        allow(threema_double).to receive(:send).with(expected_params).and_return(message_id)
+      end
+
+      it "saves the returned message id to the message's external_id" do
+        expect { subject.call }.to change { message.reload.external_id }.from(nil).to(message_id)
+      end
     end
   end
 end
--- spec/adapters/threema_adapter/outbound/text_spec.rb
+++ spec/adapters/threema_adapter/outbound/text_spec.rb
@@ -9,12 +9,24 @@ RSpec.describe ThreemaAdapter::Outbound::Text do
     build(:contributor, threema_id: threema_id, email: nil).tap { |contributor| contributor.save(validate: false) }
   end
   let(:message) { create(:message, recipient: contributor) }
+  let(:threema_double) { instance_double(Threema) }
+  let(:threema_lookup_double) { instance_double(Threema::Lookup) }
+  let(:message_id) { SecureRandom.alphanumeric(16) }
+
+  before do
+    allow(ThreemaAdapter::Outbound::Text).to receive(:threema_instance).and_return(threema_double)
+    allow(Threema::Lookup).to receive(:new).and_call_original
+    allow(Threema::Lookup).to receive(:new).with({ threema: threema_double }).and_return(threema_lookup_double)
+    allow(threema_lookup_double).to receive(:key).and_return('PUBLIC_KEY_HEX_ENCODED')
+    allow(threema_double).to receive(:send).with(type: :text, threema_id: threema_id, text: message.text).and_return(message_id)
+  end
 
   describe '#perform' do
     subject { -> { adapter.perform(text: message.text, contributor_id: message.recipient.id) } }
 
     it 'sends the message' do
-      expect_any_instance_of(Threema).to receive(:send).with(type: :text, threema_id: 'V5EA564T', text: message.text)
+      expect(threema_double).to receive(:send).with(type: :text, threema_id: threema_id, text: message.text)
+
       subject.call
     end
 
@@ -22,8 +34,17 @@ RSpec.describe ThreemaAdapter::Outbound::Text do
       let(:threema_id) { 'v5ea564t' }
 
       it 'converts ID to uppercase' do
-        expect_any_instance_of(Threema).to receive(:send).with(type: :text, threema_id: 'V5EA564T', text: message.text)
+        expect(threema_double).to receive(:send).with(type: :text, threema_id: threema_id.upcase, text: message.text)
+
         subject.call
+      end
+    end
+
+    context 'when a message is passed in' do
+      subject { -> { adapter.perform(text: message.text, contributor_id: message.recipient.id, message: message) } }
+
+      it "saves the returned message id to the message's external_id" do
+        expect { subject.call }.to change { message.reload.external_id }.from(nil).to(message_id)
       end
     end
   end
