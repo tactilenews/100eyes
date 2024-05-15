d6d940402a11665c5880b9e1bbaa461fec09579f
Samuel Oey <so@capinside.com>
2024-05-15 18:14:30 +0200

    spec: testing for reply_to input in chat form

--- spec/components/chat_form_spec.rb
+++ spec/components/chat_form_spec.rb
@@ -7,4 +7,20 @@ RSpec.describe ChatForm::ChatForm, type: :component do
 
   let(:params) { { contributor: build(:contributor, id: 42) } }
   it { should have_css('.ChatForm') }
+
+  context 'without reply_to set' do
+    it 'should not have a hidden reply_to input field' do
+      expect(subject).not_to have_css('input#message_reply_to_id', visible: false)
+    end
+  end
+
+  context 'with reply_to set' do
+    before do
+      params[:reply_to] = create(:message)
+    end
+
+    it 'should have a hidden reply_to input field' do
+      expect(subject).to have_css('input#message_reply_to_id', visible: false)
+    end
+  end
 end
