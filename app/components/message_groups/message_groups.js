0908732fbc31a6f2a50e19c48e9ada51a9e97e22
mattwr18 <mattwr18@gmail.com>
2024-05-22 11:41:02 +0200

    Add comment to explain adding css styling in js

--- app/components/message_groups/message_groups.js
+++ app/components/message_groups/message_groups.js
@@ -11,6 +11,9 @@ export default class extends Controller {
       if (!elementToScrollTo) return;
 
       elementToScrollTo.scrollIntoView({ behavior: 'smooth', block: 'center' });
+      // This was added to mimic the styles in ChatMessage:target
+      // See https://developer.mozilla.org/en-US/docs/Web/CSS/:target
+      // and https://github.com/tactilenews/100eyes/pull/1852#discussion_r1590751639
       Object.assign(elementToScrollTo.style, {
         borderColor: '#007eff',
         boxShadow: '0 0 0 3px #7ac8ff',
