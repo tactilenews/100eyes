74f0ff89bb8cc4d94f1ce6c225d60cb6bbb56f2f
Samuel Oey <so@capinside.com>
2024-05-24 21:48:07 +0200

    fix: adds eager loading to prevent n+1 queries

--- app/controllers/contributors_controller.rb
+++ app/controllers/contributors_controller.rb
@@ -62,7 +62,7 @@ class ContributorsController < ApplicationController
   end
 
   def conversations
-    @messages = @contributor.conversations
+    @messages = @contributor.conversations.includes(%i[files photos request recipient sender])
     @reply_to = @contributor.replies.find(params[:reply_to]) if params[:reply_to].present?
   end
 
