d4fc6047bdec576225e2a2957382568c60e46a48
mattwr18 <mattwr18@gmail.com>
2024-06-20 11:30:45 +0200

    Fix users factories for seeding db

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,15 +2,9 @@
 
 FactoryBot.define do
   factory :user do
-    sequence :email do |n|
-      "user#{n}@example.org"
-    end
-    sequence :first_name do |n|
-      "FirstName#{n}"
-    end
-    sequence :last_name do |n|
-      "LastName#{n}"
-    end
+    email { Faker::Internet.email }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
     password { Faker::Internet.password(min_length: 20, max_length: 128) }
     otp_enabled { true }
   end
