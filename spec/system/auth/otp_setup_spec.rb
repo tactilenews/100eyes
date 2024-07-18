a00c1645b022d5f88b97633e9f471a638dbbe989
Matthew Rider <mattwr18@gmail.com>
2024-07-18 12:41:18 +0200

    Remove code creep

--- spec/system/auth/otp_setup_spec.rb
+++ spec/system/auth/otp_setup_spec.rb
@@ -3,7 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe 'OTP Setup' do
-  let(:organization) { create(:organization) }
   let(:email) { Faker::Internet.email }
   let(:password) { Faker::Internet.password(min_length: 8, max_length: 128) }
   let(:new_password) { Faker::Internet.password(min_length: 8, max_length: 128) }
--- spec/system/dashboard/activity_notifications_spec.rb
+++ spec/system/dashboard/activity_notifications_spec.rb
@@ -4,7 +4,6 @@ require 'rails_helper'
 
 RSpec.describe 'Activity Notifications' do
   context 'with recent activity' do
-    let(:organization) { create(:organization) }
     let(:email) { Faker::Internet.email }
     let(:coworker_email) { Faker::Internet.email }
     let(:password) { Faker::Internet.password(min_length: 8, max_length: 128) }
