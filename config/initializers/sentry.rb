9a58b88482794601a702b033af588fbf84b53a1f
Matthew Rider <mattwr18@gmail.com>
2024-07-23 11:30:46 +0200

    Report ActiveRecord::RecordNotFound errors to Sentry
    these errors are excluded by default with sentry-ruby

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -4,5 +4,6 @@ Sentry.init do |config|
   config.breadcrumbs_logger = [:active_support_logger]
   config.capture_exception_frame_locals = true
   config.server_name = ENV['APPLICATION_HOSTNAME'] if ENV['APPLICATION_HOSTNAME'].present?
+  config.excluded_exceptions -= ['ActiveRecord::RecordNotFound']
   config.excluded_exceptions += ['ErrorNotifier::IgnoredError']
 end
