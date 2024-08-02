851368e7a17fb317efd4c68ef7617eed46d0413a
Samuel Oey <so@capinside.com>
2024-08-02 14:10:23 +0200

    fix: fix maybe unused hero component

--- app/components/hero/hero.rb
+++ app/components/hero/hero.rb
@@ -3,5 +3,10 @@
 module Hero
   # FIXME: this component seems to be unused
   class Hero < ApplicationComponent
+    def initialize(organization_id:, **)
+      @organization_id = organization_id
+      super
+    end
+    attr_reader :organization_id
   end
 end
--- spec/components/hero_spec.rb
+++ spec/components/hero_spec.rb
@@ -3,8 +3,9 @@
 require 'rails_helper'
 
 RSpec.describe Hero::Hero, type: :component do
-  subject { render_inline(described_class.new(**params)) }
+  subject { render_inline(described_class.new(organization_id: organization, **params)) }
 
+  let(:organization) { create(:organization) }
   let(:params) { {} }
   it { should have_css('.Hero') }
 end
