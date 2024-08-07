5206a17ff1b816e64b3d96ff33dba36ef26fa49a
Samuel Oey <so@capinside.com>
2024-08-07 19:18:26 +0200

    fix: literal string

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class OrganizationsController < ApplicationController
   def index
     redirect_to organization_dashboard_path(current_user.organization) if current_user.organization.present?
