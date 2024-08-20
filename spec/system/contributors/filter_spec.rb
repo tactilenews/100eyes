760dc9920d2c52fe54df5d398eba745c286c8d7b
Samuel Oey <samuel.oey@gmail.com>
2024-08-20 15:59:19 +0200

    Feat: scope message routes (#1966)
    Co-authored-by: Robert Schäfer <git@roschaefer.de>

--- spec/system/contributors/filter_spec.rb
+++ spec/system/contributors/filter_spec.rb
@@ -47,7 +47,7 @@ RSpec.describe 'Filter contributors' do
     expect(page).to have_link(nil, href: organization_contributor_path(organization, active_contributor))
     expect(page).not_to have_link(nil, href: organization_contributor_path(organization, another_contributor))
 
-    click_on 'leeren'
+    click_on 'zurücksetzen'
 
     expect(page).not_to have_css('.ContributorsIndex-filterSection')
     expect(page).not_to have_content('Das sind die Mitglieder deiner Community gefiltert nach dem Tag (entwickler)')
