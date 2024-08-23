73df8a6871b16d65d6bd42b181985cb53c460d4f
Samuel Oey <so@capinside.com>
2024-08-23 12:16:34 +0200

    feat: more drafting

--- spec/models/contributors/index_data_spec.rb
+++ spec/models/contributors/index_data_spec.rb
@@ -4,18 +4,26 @@ require 'rails_helper'
 
 RSpec.describe Contributors::IndexData do
 
-  let(:organization) { create(:organization) }
-  let(:active_contributor) { create(:contributor, organization: organization) }
-  let(:inactive_contributor) { create(:contributor, :inactive, organization: organization) }
-  let(:other_organizations_contributor) { }
+  let!(:organization) { create(:organization) }
+  let!(:active_contributor) { create(:contributor, organization: organization) }
+  let!(:inactive_contributor) { create(:contributor, :inactive, organization: organization) }
+
 
   let(:contributor_params) { {state: active, tag_list: ["foo, bar"] } }
 
   subject { Contributors::IndexData.new(organization, contributor_params) }
 
-  describe ".active_count" do
+  describe "active_count" do
+    let!(:other_organizations_contributor) { create(:contributor) }
     it "does include the active contributors in the organization only" do
+      expect(subject.active_count).to eq(1)
+    end
+  end
 
+  describe 'inactive_count' do
+    let!(:other_organizations_contributor) { create(:contributor, :inactive) }
+    it "does include the active contributors in the organization only" do
+      expect(subject.inactive_count).to eq(1)
     end
   end
 
