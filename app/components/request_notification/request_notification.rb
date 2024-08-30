d1ef8810f0ee44806f5c3815037abb0ff29cd88d
Samuel Oey <so@capinside.com>
2024-08-30 18:51:25 +0200

    some comment referencing the spike implementation

--- app/components/request_notification/request_notification.rb
+++ app/components/request_notification/request_notification.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# TODO: https://github.com/tactilenews/100eyes/pull/1898/files#diff-03fade3e18accd28a40e50b49dd30e8f377ffb8bec7df4e170ffd0306bf44ed2
+
 module RequestNotification
   class RequestNotification < ApplicationComponent
     def initialize(request_for_info:, **)
