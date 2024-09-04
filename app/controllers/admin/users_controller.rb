2935193f2c27c2ede743a61662442fdfd1b4d54e
Matthew Rider <mattwr18@gmail.com>
2024-09-04 20:18:20 +0200

    Test drive successfully updating user's organizations

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -14,16 +14,6 @@ module Admin
       end
     end
 
-    def update
-      user = User.find(update_params[:id])
-
-      if user.update(update_params[:user])
-        redirect_to admin_user_path(user), flash: { success: 'User was successfully updated.' }
-      else
-        render :edit, status: :unprocessable_entity
-      end
-    end
-
     private
 
     def resource_params
@@ -33,10 +23,6 @@ module Admin
 
       new_password = SecureRandom.alphanumeric(12)
       params.merge(password: new_password)
-    end
-
-    def update_params
-      params.permit(:id, user: %i[first_name last_name email admin otp_enabled active])
     end
   end
 end
