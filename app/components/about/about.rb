ce1849defc9a1a5d05c3449fe532a13ec673046b
Samuel Oey <so@capinside.com>
2024-09-05 07:56:44 +0200

    fix: linting

--- app/components/about/about.rb
+++ app/components/about/about.rb
@@ -22,26 +22,22 @@ module About
 
     def channel_status(channel)
       set_up = case channel
-      when :email
-        organization.email_from_address
-      when :threema
-        organization.threemarb_api_identity
-      when :signal
-        organization.signal_server_phone_number&.phony_formatted
-      when :telegram
-        organization.telegram_bot_username
-      when :whats_app
-        organization.whats_app_server_phone_number&.phony_formatted
-      end
+               when :email
+                 organization.email_from_address
+               when :threema
+                 organization.threemarb_api_identity
+               when :signal
+                 organization.signal_server_phone_number&.phony_formatted
+               when :telegram
+                 organization.telegram_bot_username
+               when :whats_app
+                 organization.whats_app_server_phone_number&.phony_formatted
+               end
       "#{set_up || t('.not_set_up')}: (#{channel_active(channel)})"
     end
 
     def channel_active(channel)
-      organization.channels_onboarding_allowed.include?(channel) ? t(".active") : t(".inactive")
+      organization.channels_onboarding_allowed.include?(channel) ? t('.active') : t('.inactive')
     end
-
-
-
-
   end
 end
