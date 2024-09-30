f66f81915c3d531a354165061bddf42c62635c3f
Matthew Rider <mattwr18@gmail.com>
2024-09-30 10:42:53 +0200

    Fix image uploading/sending for 360dialog

--- app/jobs/whats_app_adapter/three_sixty_dialog/upload_file.rb
+++ app/jobs/whats_app_adapter/three_sixty_dialog/upload_file.rb
@@ -5,43 +5,41 @@ require 'net/http'
 module WhatsAppAdapter
   module ThreeSixtyDialog
     class UploadFile < ApplicationJob
-      # rubocop:disable Metrics/AbcSize
       def perform(message_id:)
         @message_id = message_id
         message = Message.find_by(id: message_id)
-        return unless message
 
         request = message.request
         organization = Organization.find_by(id: request.organization.id)
-        return unless organization && organization.three_sixty_dialog_client_api_key.blank?
 
         request.files.each do |file|
-          base_uri = ENV.fetch('THREE_SIXTY_DIALOG_PARTNER_REST_API_ENDPOINT', 'https://stoplight.io/mocks/360dialog/360dialog-partner-api/24588693')
+          base_uri = ENV.fetch('THREE_SIXTY_DIALOG_WHATS_APP_REST_API_ENDPOINT', 'https://stoplight.io/mocks/360dialog/360dialog-partner-api/24588693')
           url = URI.parse("#{base_uri}/media")
           headers = {
-            'D360-API-KEY': organization.three_sixty_dialog_client_api_key,
-            'Content-Type': file.blob.content_type
+            'D360-API-KEY' => organization.three_sixty_dialog_client_api_key
           }
-          request = Net::HTTP::Post.new(url.to_s, headers)
-          request.body = File.read(ActiveStorage::Blob.service.path_for(file.blob.key))
+          params = {
+            'messaging_product' => 'whatsapp',
+            'file' => UploadIO.new(ActiveStorage::Blob.service.path_for(file.blob.key), file.blob.content_type)
+          }
+          request = Net::HTTP::Post::Multipart.new(url, params, headers)
           response = Net::HTTP.start(url.host, url.port, use_ssl: true) do |http|
             http.request(request)
           end
           handle_response(response)
         end
       end
-      # rubocop:enable Metrics/AbcSize
 
       private
 
       attr_reader :message_id
 
       def handle_response(response)
-        case response.code.to_i
-        when 201
-          file_id = JSON.parse(response.body)['media'].first['id']
+        case response
+        when Net::HTTPSuccess
+          file_id = JSON.parse(response.body)['id']
           WhatsAppAdapter::ThreeSixtyDialogOutbound::File.perform_later(message_id: message_id, file_id: file_id)
-        when 400..599
+        when Net::HTTPClientError, Net::HTTPServerError
           exception = WhatsAppAdapter::ThreeSixtyDialogError.new(error_code: response.code, message: response.body)
           ErrorNotifier.report(exception)
         end
