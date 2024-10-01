b15a96d8fd0a1b302620ab747542945b2bb5cf71
Matthew Rider <mattwr18@gmail.com>
2024-10-01 11:55:23 +0200

    Mock base_uri in spec

--- spec/jobs/whats_app_adapter/three_sixty_dialog/upload_file_job_spec.rb
+++ spec/jobs/whats_app_adapter/three_sixty_dialog/upload_file_job_spec.rb
@@ -10,6 +10,12 @@ RSpec.describe WhatsAppAdapter::ThreeSixtyDialog::UploadFileJob do
     let(:message) { create(:message, request: create(:request, :with_file, organization: organization)) }
     let(:external_file_id) { '545466424653131' }
 
+    before do
+      allow(ENV).to receive(:fetch).with(
+        ENV.fetch('THREE_SIXTY_DIALOG_WHATS_APP_REST_API_ENDPOINT', 'https://stoplight.io/mocks/360dialog/360dialog-partner-api/24588693')
+      ).and_return('https://waba-v2.360dialog.io')
+    end
+
     it 'schedules a job to send out the message with the file', vcr: { cassette_name: :three_sixty_dialog_upload_file_job } do
       expect { subject.call }.to have_enqueued_job(
         WhatsAppAdapter::ThreeSixtyDialogOutbound::File
