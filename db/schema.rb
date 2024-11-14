ba882997b2f1c36e49fbdeeb29c7990c5cd25ffc
Matthew Rider <mattwr18@gmail.com>
2024-11-14 10:23:59 +0100

    Fix german grammar in more infos messsage (#2074)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_10_10_111754) do
+ActiveRecord::Schema.define(version: 2024_11_13_120655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
@@ -196,7 +196,7 @@ ActiveRecord::Schema.define(version: 2024_10_10_111754) do
     t.string "onboarding_data_processing_consent_additional_info", default: ""
     t.string "onboarding_page", default: "Wir freuen uns, dass Sie an unserer Dialog-Recherche zur Schweinehaltung in Deutschland teilnehmen. Denn dort zeigen sich überall schwerwiegende Probleme: Schlachthofskandale, menschenunwürdige Arbeitsbedingungen, Tierqualen, dazu ein rapider Preisverfall, der Schweinebauern ruiniert. So kann es nicht weitergehen. Das weiß jeder, der mit diesem Markt zu tun hat.\n\nLösungsvorschläge gibt es bisher keine. Deswegen starten wir diese Dialog-Recherche mit Ihrer Hilfe. Statt mit ein bis drei Betroffenen oder Experten ein Interview zu führen und dann einen Beitrag zu veröffentlichen, wollen wir mit vielen verschiedenen Menschen das Gespräch suchen. Und das über einen Zeitraum von vier Wochen. Damit wir ein breiteres Bild bekommen und Dinge besser verstehen.\n\nEs ist ein Experiment. Danke, dass Sie sich darauf einlassen.\n\n### Wer?\nHinter dem #Schweinesystem stecken Menschen, keine Maschinen: Die Journalisten Katharina Jakob und Jens Eber, der Filmemacher Oliver Eberhardt sowie das Dialog-Recherche-Team Isabelle Buckow, Astrid Csuraji und Dr. Jakob Vicari von tactile.news. [Auf unserer Webseite](https://tactile.news/ueber-uns) stellen wir unsere Idee vom Dialog-Journalismus ausführlicher vor.\n\n### Wie?\nWir stellen Ihnen in den nächsten vier, fünf Wochen immer mal wieder eine Frage. Am liebsten per Telegram Messenger. Wenn Sie den nicht nutzen und auch nicht installieren wollen (er ist sehr ähnlich wie WhatsApp), kommunizieren Sie mit uns per E-Mail. Es bleibt Ihnen überlassen, ob Sie jede Frage beantworten oder auch welche auslassen.\n\n### Wo?\nDamit wir Sie künftig auf dem Kanal Ihrer Wahl erreichen, teilen Sie uns bitte mit, ob wir Sie per E-Mail oder Telegram anschreiben sollen.\n"
     t.string "onboarding_success_heading", default: "Vielen Dank für Ihre Anmeldung bei 100eyes.\n"
-    t.string "onboarding_success_text", default: "Unsere Dialog-Recherche startet bald. Wir melden uns dann bei Ihnen.\n\nUm unseren Kanal Abzubestellen, schreibe „abbestellen“.\n"
+    t.string "onboarding_success_text", default: "Unsere Dialog-Recherche startet bald. Wir melden uns dann bei Ihnen.\n\nUm unseren Kanal abzubestellen, schreibe „abbestellen“.\n"
     t.string "onboarding_unauthorized_heading", default: "Leider ist dieser Einladungs-Link nicht mehr gültig.\n"
     t.string "onboarding_unauthorized_text", default: "Das ist aber nicht Ihre Schuld! Bitte kontaktieren Sie uns unter support@100ey.es, um einen neuen Link zu erhalten.\n\n"
     t.string "onboarding_data_protection_link", default: "https://tactile.news/100eyes-datenschutz/"
