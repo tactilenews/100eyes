9114677a7467bbcaa99e7c548dea5464d8123e85
Matthew Rider <mattwr18@gmail.com>
2025-01-08 13:43:08 +0100

    Mock signal cli rest api endpoint

--- spec/jobs/signal_adapter/set_username_job_spec.rb
+++ spec/jobs/signal_adapter/set_username_job_spec.rb
@@ -9,6 +9,12 @@ RSpec.describe SignalAdapter::SetUsernameJob do
 
     let(:organization) { create(:organization, signal_server_phone_number: '+4912345678') }
 
+    before do
+      allow(ENV).to receive(:fetch).with(
+        'SIGNAL_CLI_REST_API_ENDPOINT', 'http://localhost:8080'
+      ).and_return('http://signal:8080')
+    end
+
     it 'updates the signal_complete_onboarding_link', vcr: { cassette_name: :signal_set_username } do
       expect { subject.call }.to (change { organization.reload.signal_complete_onboarding_link }).from(nil).to('https://signal.me/some_valid_uuid')
     end
