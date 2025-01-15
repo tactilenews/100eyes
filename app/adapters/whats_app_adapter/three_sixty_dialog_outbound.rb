3f354295c16ea63684de7cc02039e2d88668ad6b
Matthew Rider <mattwr18@gmail.com>
2025-01-15 11:04:23 +0100

    Allow Outbound Job to determine payload

--- app/adapters/whats_app_adapter/three_sixty_dialog_outbound.rb
+++ app/adapters/whats_app_adapter/three_sixty_dialog_outbound.rb
@@ -1,191 +1,95 @@
 # frozen_string_literal: true
 
-# rubocop:disable Metrics/ClassLength
 module WhatsAppAdapter
   class ThreeSixtyDialogOutbound
     class << self
       def send!(message)
-        recipient = message&.recipient
-        return unless contributor_can_receive_messages?(recipient)
+        contributor = message&.recipient
+        return unless contributor_can_receive_messages?(contributor)
 
-        if freeform_message_permitted?(recipient)
-          send_message(recipient, message)
+        if freeform_message_permitted?(contributor)
+          send_message(message)
         else
-          send_message_template(recipient, message)
+          send_message_template(message)
         end
       end
 
-      def send_welcome_message!(contributor, organization)
+      def send_welcome_message!(contributor)
         return unless contributor_can_receive_messages?(contributor)
 
-        welcome_message = ["*#{organization.onboarding_success_heading}*", organization.onboarding_success_text].join("\n\n")
-        payload = if freeform_message_permitted?(contributor)
-                    text_payload(contributor, welcome_message)
-                  else
-                    welcome_message_payload(contributor, organization)
-                  end
-        WhatsAppAdapter::ThreeSixtyDialogOutbound::Text.perform_later(organization_id: organization.id, payload: payload)
+        if freeform_message_permitted?(contributor)
+          organization = contributor.organization
+          welcome_message = ["*#{organization.onboarding_success_heading}*", organization.onboarding_success_text].join("\n\n")
+          WhatsAppAdapter::ThreeSixtyDialogOutbound::Text.perform_later(contributor_id: contributor.id, type: :text,
+                                                                        text: welcome_message)
+        else
+          WhatsAppAdapter::ThreeSixtyDialogOutbound::Text.perform_later(contributor_id: contributor.id, type: :welcome_message_template)
+        end
       end
 
-      def send_unsupported_content_message!(contributor, organization)
+      def send_unsupported_content_message!(contributor)
         return unless contributor_can_receive_messages?(contributor)
 
         text = I18n.t('adapter.whats_app.unsupported_content_template', first_name: contributor.first_name,
                                                                         contact_person: contributor.organization.contact_person.name)
-        WhatsAppAdapter::ThreeSixtyDialogOutbound::Text.perform_later(organization_id: organization.id,
-                                                                      payload: text_payload(
-                                                                        contributor, text
-                                                                      ))
+        WhatsAppAdapter::ThreeSixtyDialogOutbound::Text.perform_later(contributor_id: contributor.id, type: :text, text: text)
       end
 
-      def send_more_info_message!(contributor, organization)
+      def send_more_info_message!(contributor)
         return unless contributor_can_receive_messages?(contributor)
 
-        WhatsAppAdapter::ThreeSixtyDialogOutbound::Text.perform_later(organization_id: organization.id,
-                                                                      payload: text_payload(
-                                                                        contributor, organization.whats_app_more_info_message
-                                                                      ))
+        WhatsAppAdapter::ThreeSixtyDialogOutbound::Text.perform_later(contributor_id: contributor.id, type: :text,
+                                                                      text: organization.whats_app_more_info_message)
       end
 
-      def send_unsubsribed_successfully_message!(contributor, organization)
+      def send_unsubsribed_successfully_message!(contributor)
         return unless contributor_can_receive_messages?(contributor)
 
         text = [I18n.t('adapter.shared.unsubscribe.successful'),
                 "_#{I18n.t('adapter.shared.resubscribe.instructions')}_"].join("\n\n")
 
-        WhatsAppAdapter::ThreeSixtyDialogOutbound::Text.perform_later(organization_id: organization.id,
-                                                                      payload: text_payload(
-                                                                        contributor, text
-                                                                      ))
+        WhatsAppAdapter::ThreeSixtyDialogOutbound::Text.perform_later(contributor_id: contributor.id, type: :text, text: text)
       end
 
-      def send_resubscribe_error_message!(contributor, organization)
+      def send_resubscribe_error_message!(contributor)
         return unless contributor_can_receive_messages?(contributor)
 
         text = I18n.t('adapter.shared.resubscribe.failure')
-        WhatsAppAdapter::ThreeSixtyDialogOutbound::Text.perform_later(organization_id: organization.id,
-                                                                      payload: text_payload(
-                                                                        contributor, text
-                                                                      ))
+        WhatsAppAdapter::ThreeSixtyDialogOutbound::Text.perform_later(contributor_id: organization.id, type: :text, text: text)
       end
 
       private
 
-      def contributor_can_receive_messages?(recipient)
-        recipient&.whats_app_phone_number.present?
+      def contributor_can_receive_messages?(contributor)
+        contributor&.whats_app_phone_number.present?
       end
 
-      def time_of_day
-        current_time = Time.current
-        morning = current_time.change(hour: 6)
-        day = current_time.change(hour: 11)
-        evening = current_time.change(hour: 17)
-        night = current_time.change(hour: 23)
-
-        case current_time
-        when morning..day
-          'morning'
-        when day..evening
-          'day'
-        when evening..night
-          'evening'
-        else
-          'night'
-        end
-      end
-
-      def freeform_message_permitted?(recipient)
-        responding_to_template_message = recipient.whats_app_message_template_responded_at.present? &&
-                                         recipient.whats_app_message_template_responded_at > 24.hours.ago
-        latest_message_received_within_last_24_hours = recipient.replies.first&.created_at.present? &&
-                                                       recipient.replies.first.created_at > 24.hours.ago
+      def freeform_message_permitted?(contributor)
+        responding_to_template_message = contributor.whats_app_message_template_responded_at.present? &&
+                                         contributor.whats_app_message_template_responded_at > 24.hours.ago
+        latest_message_received_within_last_24_hours = contributor.replies.first&.created_at.present? &&
+                                                       contributor.replies.first.created_at > 24.hours.ago
         responding_to_template_message || latest_message_received_within_last_24_hours
       end
 
-      def send_message_template(recipient, message)
-        recipient.update(whats_app_message_template_sent_at: Time.current)
-        WhatsAppAdapter::ThreeSixtyDialogOutbound::Text.perform_later(organization_id: message.organization.id,
-                                                                      payload: new_request_payload(
-                                                                        recipient, message.request
-                                                                      ),
+      def send_message_template(message)
+        contributor.update(whats_app_message_template_sent_at: Time.current)
+        WhatsAppAdapter::ThreeSixtyDialogOutbound::Text.perform_later(contributor_id: message.recipient.id,
+                                                                      type: :request_template,
                                                                       message_id: message.id)
       end
 
-      def send_message(recipient, message)
+      def send_message(message)
         if message.files.present?
           WhatsAppAdapter::ThreeSixtyDialogOutbound::File.perform_later(message_id: message.id)
 
         else
 
-          WhatsAppAdapter::ThreeSixtyDialogOutbound::Text.perform_later(organization_id: message.organization.id,
-                                                                        payload: text_payload(
-                                                                          recipient, message.text
-                                                                        ),
+          WhatsAppAdapter::ThreeSixtyDialogOutbound::Text.perform_later(contributor_id: message.recipient.id,
+                                                                        type: :text,
                                                                         message_id: message.id)
         end
-      end
-
-      # rubocop:disable Metrics/MethodLength
-      def new_request_payload(recipient, request)
-        {
-          messaging_product: 'whatsapp',
-          recipient_type: 'individual',
-          to: recipient.whats_app_phone_number.split('+').last,
-          type: 'template',
-          template: {
-            language: {
-              policy: 'deterministic',
-              code: 'de'
-            },
-            name: "new_request_#{time_of_day}_#{rand(1..3)}",
-            components: [
-              {
-                type: 'body',
-                parameters: [
-                  {
-                    type: 'text',
-                    text: recipient.first_name
-                  },
-                  {
-                    type: 'text',
-                    text: request.title
-                  }
-                ]
-              }
-            ]
-          }
-        }
-      end
-      # rubocop:enable Metrics/MethodLength
-
-      def text_payload(recipient, text)
-        {
-          messaging_product: 'whatsapp',
-          recipient_type: 'individual',
-          to: recipient.whats_app_phone_number.split('+').last,
-          type: 'text',
-          text: {
-            body: text
-          }
-        }
-      end
-
-      def welcome_message_payload(recipient, organization)
-        {
-          messaging_product: 'whatsapp',
-          recipient_type: 'individual',
-          to: recipient.whats_app_phone_number.split('+').last,
-          type: 'template',
-          template: {
-            language: {
-              policy: 'deterministic',
-              code: 'de'
-            },
-            name: "welcome_message_#{organization.project_name.parameterize.underscore}"
-          }
-        }
       end
     end
   end
 end
-# rubocop:enable Metrics/ClassLength
--- app/adapters/whats_app_adapter/three_sixty_dialog_outbound/text.rb
+++ app/adapters/whats_app_adapter/three_sixty_dialog_outbound/text.rb
@@ -13,13 +13,15 @@ module WhatsAppAdapter
         end
       end
 
-      def perform(organization_id:, payload:, message_id: nil)
+      def perform(contributor_id:, type:, text: nil, message_id: nil)
+        @contributor = Contributor.find(contributor_id)
+        @type = type
+
+        @text = text
         @message = Message.find(message_id) if message_id
-        @payload = payload
-        organization = Organization.find(organization_id)
 
         url = URI.parse("#{ENV.fetch('THREE_SIXTY_DIALOG_WHATS_APP_REST_API_ENDPOINT', 'https://stoplight.io/mocks/360dialog/360dialog-partner-api/24588693')}/messages")
-        headers = { 'D360-API-KEY' => organization.three_sixty_dialog_client_api_key, 'Content-Type' => 'application/json' }
+        headers = { 'D360-API-KEY' => contributor.organization.three_sixty_dialog_client_api_key, 'Content-Type' => 'application/json' }
         request = Net::HTTP::Post.new(url, headers)
 
         request.body = payload.to_json
@@ -29,9 +31,95 @@ module WhatsAppAdapter
         handle_response(response)
       end
 
-      attr_reader :message, :payload
+      attr_reader :contributor, :type, :text, :message
 
       private
+
+      def payload
+        case type
+        when :text
+          text_payload
+        when :welcome_message_template
+          welcome_message_payload
+        when :request_template
+          request_payload
+        end
+      end
+
+      def text_payload
+        base_payload.merge({
+                             type: 'text',
+                             text: {
+                               body: text || message.text
+                             }
+                           })
+      end
+
+      def welcome_message_payload
+        template_name = "welcome_message_#{contributor.organization.project_name.parameterize.underscore}"
+        template_payload(template_name)
+      end
+
+      def time_of_day
+        current_time = Time.current
+        morning = current_time.change(hour: 6)
+        day = current_time.change(hour: 11)
+        evening = current_time.change(hour: 17)
+        night = current_time.change(hour: 23)
+
+        case current_time
+        when morning..day
+          'morning'
+        when day..evening
+          'day'
+        when evening..night
+          'evening'
+        else
+          'night'
+        end
+      end
+
+      def new_request_payload
+        template_name = "new_request_#{time_of_day}_#{rand(1..3)}"
+        template_payload(template_name).merge({
+                                                components: [
+                                                  {
+                                                    type: 'body',
+                                                    parameters: [
+                                                      {
+                                                        type: 'text',
+                                                        text: contributor.first_name
+                                                      },
+                                                      {
+                                                        type: 'text',
+                                                        text: message.request.title
+                                                      }
+                                                    ]
+                                                  }
+                                                ]
+                                              })
+      end
+
+      def template_payload(name)
+        base_payload.merge({
+                             type: 'template',
+                             template: {
+                               language: {
+                                 policy: 'deterministic',
+                                 code: 'de'
+                               },
+                               name: name
+                             }
+                           })
+      end
+
+      def base_payload
+        {
+          messaging_product: 'whatsapp',
+          recipient_type: 'individual',
+          to: contributor.whats_app_phone_number.split('+').last
+        }
+      end
 
       def handle_response(response)
         case response
