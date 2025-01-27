872408424dc23dcda0855584975680d7247dd7af
Matthew Rider <mattwr18@gmail.com>
2025-01-27 19:43:05 +0100

    Clean up after refactor (#2099)

--- app/jobs/whats_app_adapter/three_sixty_dialog/process_webhook_job.rb
+++ app/jobs/whats_app_adapter/three_sixty_dialog/process_webhook_job.rb
@@ -3,15 +3,13 @@
 module WhatsAppAdapter
   module ThreeSixtyDialog
     class ProcessWebhookJob < ApplicationJob
-      attr_reader :organization, :whats_app_message
-
       def perform(organization_id:, components:)
-        @organization = Organization.find(organization_id)
+        organization = Organization.find(organization_id)
 
-        @whats_app_message = components
+        whats_app_message = components
         adapter = WhatsAppAdapter::ThreeSixtyDialogInbound.new
 
-        adapter.consume(organization, components)
+        adapter.consume(organization, whats_app_message)
       end
     end
   end
