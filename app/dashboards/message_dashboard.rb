1420e28cd3fb8f9e5388fc11ebf1e9515025bb75
Matthew Rider <mattwr18@gmail.com>
2025-02-14 15:18:25 +0100

    Add searchability by org
    Closes #2114

--- app/dashboards/message_dashboard.rb
+++ app/dashboards/message_dashboard.rb
@@ -12,16 +12,30 @@ class MessageDashboard < Administrate::BaseDashboard
     request: Field::BelongsTo,
     created_at: Field::DateTime,
     text: Field::Text,
-    raw_data: RawDataField
+    raw_data: RawDataField,
+    organization: Field::BelongsTo.with_options(
+      searchable: true,
+      searchable_fields: %w[name project_name]
+    )
   }.freeze
 
   COLLECTION_ATTRIBUTES = %i[
+    organization
     id
     sender
     recipient
     request
     text
     created_at
+  ].freeze
+
+  SHOW_PAGE_ATTRIBUTES = %i[
+    organization
+    id
+    sender
+    text
+    created_at
+    updated_at
   ].freeze
 
   SHOW_PAGE_ATTRIBUTES = ATTRIBUTE_TYPES.keys
