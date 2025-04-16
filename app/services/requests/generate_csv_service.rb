f464b34f4bab4191472ff0b051d70710896d5b20
Matthew Rider <mattwr18@gmail.com>
2025-04-16 11:30:33 +0200

    Update columns to unify some info
    and clarify which row is the request

--- app/services/requests/generate_csv_service.rb
+++ app/services/requests/generate_csv_service.rb
@@ -6,14 +6,14 @@ module Requests
 
     def initialize(request_id:)
       @request = Request.find(request_id)
-      @headers = ['ID', 'Name', 'Text', 'Gesendet am']
+      @headers = ['ID', 'SENDER', 'TEXT']
     end
 
     def call
       CSV.generate(write_headers: true, headers: headers) do |writer|
-        writer << [request.id, request.user.name, request.text, request.broadcasted_at.strftime('%Y-%m-%d um %H:%M')]
+        writer << [request.id, I18n.t('service.requests.generate_csv_service.sender_info.request', sender_name: request.user.name, sent_at: request.broadcasted_at.strftime('%Y-%m-%d um %H:%M')), request.text]
         request.messages.includes(:sender).where(broadcasted: false).where.not(text: [nil, '']).reverse_order.each do |message|
-          writer << [message.id, message.sender.name, message.text, message.created_at.strftime('%Y-%m-%d um %H:%M')]
+          writer << [message.id, I18n.t('service.requests.generate_csv_service.sender_info.request', sender_name: message.sender.name, sent_at:  message.created_at.strftime('%Y-%m-%d um %H:%M')), message.text]
         end
       end
     end
