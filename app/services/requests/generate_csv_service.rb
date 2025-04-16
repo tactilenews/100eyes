ee399428e07bb0df723f3c8d15fb8f8d55e99528
Matthew Rider <mattwr18@gmail.com>
2025-04-16 14:01:45 +0200

    Display a message when a file is attached

--- app/services/requests/generate_csv_service.rb
+++ app/services/requests/generate_csv_service.rb
@@ -6,16 +6,33 @@ module Requests
 
     def initialize(request_id:)
       @request = Request.find(request_id)
-      @headers = ['ID', 'SENDER', 'TEXT']
+      @headers = %w[ID SENDER TEXT]
     end
 
+    # rubocop:disable Metrics/AbcSize
     def call
       CSV.generate(write_headers: true, headers: headers) do |writer|
-        writer << [request.id, I18n.t('service.requests.generate_csv_service.sender_info.request', sender_name: request.user.name, sent_at: request.broadcasted_at.strftime('%Y-%m-%d um %H:%M')), request.text]
-        request.messages.includes(:sender).where(broadcasted: false).where.not(text: [nil, '']).reverse_order.each do |message|
-          writer << [message.id, I18n.t('service.requests.generate_csv_service.sender_info.request', sender_name: message.sender.name, sent_at:  message.created_at.strftime('%Y-%m-%d um %H:%M')), message.text]
+        writer << [request.id,
+                   I18n.t('service.requests.generate_csv_service.sender_info.request',
+                          sender_name: request.user.name,
+                          sent_at: request.broadcasted_at.strftime('%Y.%m.%d um %H:%M')),
+                   request.text]
+        request.messages.includes(:sender).where(broadcasted: false).reverse_order.each do |message|
+          text = if message.text.present? && message.files.any? { |file| file.attachment.attached? }
+                   "#{I18n.t('service.requests.generate_csv_service.files_attached_with_caption')}\n#{message.text}"
+                 elsif message.text.blank?
+                   I18n.t('service.requests.generate_csv_service.files_attached')
+                 else
+                   message.text
+                 end
+          writer << [message.id,
+                     I18n.t('service.requests.generate_csv_service.sender_info.reply',
+                            sender_name: message.sender.name,
+                            sent_at: message.created_at.strftime('%Y.%m.%d um %H:%M')),
+                     text]
         end
       end
     end
+    # rubocop:enable Metrics/AbcSize
   end
 end
